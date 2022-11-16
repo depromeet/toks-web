2a7bd570c9acb542c7b1e15d4dc542192573476b
dengoyoon <engus927@naver.com>
2022-11-16 19:11:37 +0900

    ✨ feat: msw.sh에서 worker, hanlers의 기본파일 생성 코드 추가

--- scripts/msw.sh
+++ scripts/msw.sh
@@ -2,3 +2,16 @@
 
 yarn add -D msw
 npx msw init public/ --save
+
+mkdir mocks
+cd mocks
+touch worker.ts
+echo "import { setupServer } from 'msw/node';" >> worker.ts
+echo "import { handlers } from './handlers';" >> worker.ts
+echo "" >> worker.ts
+echo "export const server = setupServer(...handlers);" >> worker.ts
+echo "" >> worker.ts
+
+touch handlers.ts
+echo "import { rest } from 'msw';" >> handlers.ts
+echo "" >> handlers.ts
