b6f0ce0509bd570670a64d17a8081a59fad4756c
dengoyoon <engus927@naver.com>
2022-11-17 17:06:18 +0900

    ✨ feat: worker와 handlers의 생성 방식을 템플릿과 cp를 사용하는 것으로 변경

--- scripts/msw.sh
+++ scripts/msw.sh
@@ -3,18 +3,4 @@
 yarn add -D msw
 npx msw init mocks/ --save
 
-cd mocks
-touch worker.ts
-echo "import { setupServer } from 'msw/node';" >> worker.ts
-echo "import { handlers } from './handlers';" >> worker.ts
-echo "" >> worker.ts
-echo "export const server = setupServer(...handlers);" >> worker.ts
-
-touch handlers.ts
-echo "import { rest } from 'msw';" >> handlers.ts
-echo "" >> handlers.ts
-echo "export const handlers = [" >> handlers.ts
-echo "    rest.get('', (req, res, ctx) => {" >> handlers.ts
-echo "      return res(ctx.status(200), ctx.json({}));" >> handlers.ts
-echo "    })," >> handlers.ts
-echo "];" >> handlers.ts
+cp ../../_templates/msw/handlers.ts ../../_templates/msw/worker.ts ./mocks
