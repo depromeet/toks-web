4b99e7e074e47f3f8ecb44d3bee8a4e00264a1ee
dengoyoon <engus927@naver.com>
2022-11-17 19:31:25 +0900

    🎨 style: 프로필 아이콘 크기 지정

--- services/quiz/src/pages/StudyDetailPage/components/Header/ProfileIcon/ProfileIcon.tsx
+++ services/quiz/src/pages/StudyDetailPage/components/Header/ProfileIcon/ProfileIcon.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import ic_default_profile from "../../../../../img/ic_default_profile.png";
+import { ProfileIconCss } from "./style/ProfileIconCss";
 
 function ProfileIcon() {
     return (
-        <Image 
+        <Image
+            css={ProfileIconCss}
             src={ic_default_profile}
             alt="사용자 프로필 이미지"/>
     );
 }
 
 export default ProfileIcon;
-
---
+++ services/quiz/src/pages/StudyDetailPage/components/Header/ProfileIcon/style/ProfileIconCss.ts
@@ -0,0 +1,8 @@
+import { css } from "@emotion/react"
+
+const ProfileIconCss = css({
+    height: "24px",
+    width: "24px"
+})
+
+export { ProfileIconCss };
