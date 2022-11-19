766e761325511bbc8700b6f510cfbc601ec0ef64
chaaerim <cofla411@ewhain.net>
2022-11-19 16:04:16 +0900

    :sparkles: feat: import primereact css

--- packages/toks-components/src/index.ts
+++ packages/toks-components/src/index.ts
@@ -1,1 +1,5 @@
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
+import 'primereact/resources/primereact.min.css'; //core css
+import 'primeicons/primeicons.css'; //icons
+
 export * from 'primereact';
