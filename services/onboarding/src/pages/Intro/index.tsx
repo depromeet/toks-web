42e544ffa733206e45f448e7bd75f0cfdb1e2bee
minsgy <alstjrchl97@naver.com>
2022-11-20 21:27:39 +0900

    ✨ feat: onboarding service 설정

---
+++ services/onboarding/src/pages/Intro/index.tsx
@@ -0,0 +1,17 @@
+import { Image } from '@depromeet/toks-components';
+import React from 'react';
+
+function Intro() {
+  return (
+    <div
+      style={{
+        width: '200px',
+        height: '200px',
+      }}
+    >
+      <Image src="https://picsum.photos/200/300" mode="cover" alt="테스트" />
+    </div>
+  );
+}
+
+export default Intro;
