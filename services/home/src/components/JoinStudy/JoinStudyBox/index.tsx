be6c87f588ae01057e8c72b6c3729d7a2e863364
chaaerim <cofla411@ewhain.net>
2022-11-22 04:13:36 +0900

    :sparkles: feat: 컴포넌트 인터페이스 구조 잡기

--- services/home/src/components/JoinStudy/JoinStudyBox/index.tsx
+++ services/home/src/components/JoinStudy/JoinStudyBox/index.tsx
@@ -1,14 +1,12 @@
 import { JoinButton } from 'components/JoinStudy/JoinButton';
 import { JoinInput } from 'components/JoinStudy/JoinInput';
-import { DetailDescription } from 'components/JoinStudy/DetailDescription';
-import { StudyDate } from 'components/JoinStudy/StudyDate';
-import { StudyDescription } from 'components/JoinStudy/StudyDescription';
+import { StudyInfo } from 'components/JoinStudy/StudyIntro';
 import { StudyStack } from 'components/JoinStudy/StudyStack';
 import { StudyTitle } from 'components/JoinStudy/StudyTitle';
-import { DateWrapper, DescriptionContainer, JoinMessage, Wrapper } from './style';
+import { DescriptionContainer, JoinMessage, Wrapper } from './style';
 
 export function JoinStudyBox() {
-    // mock data
+  // mock data
   const ourStudyDescription = '아키텍쳐 크리너스로 소프트웨어 구조와 설계의 원칙 제대로 이해하기 ';
   const personnelDescription = '5-7명을 계획하고 있어요.';
   const startDate = '2022. 10. 13';
@@ -18,17 +16,26 @@ export function JoinStudyBox() {
       <StudyTitle />
       <StudyStack />
       <DescriptionContainer>
-        <StudyDescription descriptionText='우리 스터디는' />
-        <DetailDescription detailDescription={ourStudyDescription} />
-        <StudyDescription descriptionText='스터디 기간은' />
-        <DateWrapper>
-          <StudyDate when='시작일' date={startDate} />
-          <StudyDate when='종료일' date={doneDate} />
-        </DateWrapper>
-        <StudyDescription descriptionText='스터디 인원은' />
-        <DetailDescription detailDescription={personnelDescription} />
-        <StudyDescription descriptionText='나는 똑스 스터디에서' />
-        <JoinInput />
+        <div />
+        <StudyInfo
+          leftAddon={<div />}
+          title={<span>우리 스터디는</span>}
+          description={<span>{ourStudyDescription}</span>}
+        />
+        <StudyInfo
+          leftAddon={<div />}
+          title={<span>스터디 기간은</span>}
+          start={<>시작일</>}
+          startDate={<>{startDate}</>}
+          done={<>종료일</>}
+          doneDate={<>{doneDate}</>}
+        />
+        <StudyInfo
+          leftAddon={<div />}
+          title={<span>스터디 인원은</span>}
+          description={<span>{personnelDescription}</span>}
+        />
+        <StudyInfo leftAddon={<div />} title={<span>나는 똑스 스터디에서</span>} />
       </DescriptionContainer>
       <JoinMessage>똑스와 8주간 함께해볼까요?</JoinMessage>
       <JoinButton />
