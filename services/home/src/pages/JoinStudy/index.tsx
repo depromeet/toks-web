d5d04c24017fe0baf8402eb4406102007daffc51
chaaerim <cofla411@ewhain.net>
2022-11-23 01:42:10 +0900

    :fire: remove: Page wrapper 삭제

--- services/home/src/pages/JoinStudy/index.tsx
+++ services/home/src/pages/JoinStudy/index.tsx
@@ -1,13 +1,13 @@
 import { JoinStudyBox } from 'components/JoinStudy/JoinStudyBox';
 
-import { JoinGuide, Wrapper } from './style';
+import { JoinGuide } from './style';
 
 function JoinStudy() {
   return (
-    <Wrapper>
+    <>
       <JoinGuide>앞으로 똑스와 8주 동안 함께해볼까요?</JoinGuide>
       <JoinStudyBox />
-    </Wrapper>
+    </>
   );
 }
 
