323ab77463c5d46a0b06f1f321e589cd6665672c
goo <goo@tossbank.com>
2022-11-22 21:11:57 +0900

    feat: 글로벌 스타일 정의 (배경색, 폰트)

--- shared/toks-components/src/components/GlobalStyle/index.tsx
+++ shared/toks-components/src/components/GlobalStyle/index.tsx
@@ -1,3 +1,6 @@
+import { css, Global } from '@emotion/react';
+import { theme } from '@depromeet/theme';
+
 export function GlobalStyle() {
   return (
     <>
@@ -5,6 +8,56 @@ export function GlobalStyle() {
       <link rel="stylesheet" href="https://unpkg.com/primereact/resources/themes/lara-light-indigo/theme.css" />
       <link rel="stylesheet" href="https://unpkg.com/primereact/resources/primereact.min.css" />
       <link rel="stylesheet" href="https://unpkg.com/primeflex@3.2.1/primeflex.min.css" />
+      <link href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css" rel="stylesheet" type="text/css" />
+      <Global styles={globalCss} />
     </>
   );
 }
+
+/*
+TODO:
+1. @depromeet/theme로 옮기기
+2. color token css 변수로 정의되면 css파일로 옮겨서 관리해도 좋을듯
+3. font token 정의하기
+*/
+const globalCss = css`
+  html {
+    font-size: 16px;
+  }
+
+  body {
+    font-family: 'Spoqa Han Sans Neo', 'sans-serif';
+    font-size: 1rem;
+    box-sizing: border-box;
+    word-break: keep-all;
+    overflow-wrap: break-word;
+
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    background-color: ${theme.colors.gray110};
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  input,
+  select,
+  textarea,
+  button {
+    font-family: inherit;
+  }
+
+  body.no-scroll {
+    overflow: hidden;
+  }
+
+  a,
+  a:focus,
+  a:hover {
+    text-decoration: none;
+  }
+`;
