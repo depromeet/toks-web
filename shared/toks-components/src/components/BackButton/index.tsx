86a5f6af261e636df3e81d83e54091a799ece163
dengoyoon <engus927@naver.com>
2022-11-23 03:22:01 +0900

    ♻️ refactor: 백버튼 이미지를 Image컴포넌트 사용하는것으로 변경

--- shared/toks-components/src/components/BackButton/index.tsx
+++ shared/toks-components/src/components/BackButton/index.tsx
@@ -1,9 +1,10 @@
+import { Image } from '../Image';
 import { Button } from './style';
 
 export function BackButton() {
   return (
     <Button>
-      <img src="https://asset.tokstudy.com/ic_left_chevron.svg" alt="뒤로가기 버튼 입니다." />
+      <Image src="https://asset.tokstudy.com/ic_left_chevron.svg" alt="뒤로가기 버튼 입니다." />
     </Button>
   );
 }
