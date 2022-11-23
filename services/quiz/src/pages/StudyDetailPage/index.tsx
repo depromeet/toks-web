c9c0b5e73511c74cb819090a4fe55411b31f08fb
dengoyoon <engus927@naver.com>
2022-11-23 18:49:43 +0900

    ✨ feat: 아바타 그룹 구현 완료, 호버시 툴팁은 미구현

--- services/quiz/src/pages/StudyDetailPage/index.tsx
+++ services/quiz/src/pages/StudyDetailPage/index.tsx
@@ -1,11 +1,12 @@
-import { Tag, Avatar, BackButton, ToksHeader } from '@depromeet/toks-components';
+import { BackButton, ToksHeader } from '@depromeet/toks-components';
+
 import { QuizList } from 'components/StudyDetailPage/QuizList';
 import { Ranking } from 'components/StudyDetailPage/Ranking';
 import { StudyInfo } from 'components/StudyDetailPage/StudyInfo';
 import { StudyProgress } from 'components/StudyDetailPage/StudyProgress';
 
 import { Page, Section, Wrapper } from './style';
-//imgUrl={`https://asset.tokstudy.com/img_penguin.png`} size={"large"}
+
 export default function StudyDetailPage() {
   return (
     <Page>
@@ -18,48 +19,6 @@ export default function StudyDetailPage() {
           <StudyProgress />
           <QuizList />
           <Ranking />
-          <Tag.Row>
-            <Tag value="javascript" />
-            <Tag value="javascript" color="highlight" />
-            <Tag value="javascript" color="highlight" />
-            <Tag value="javascript" color="highlight" />
-            <Tag value="javascript" color="highlight" />
-            <Tag value="javascript" color="highlight" />
-          </Tag.Row>
-          <Avatar.Group>
-          <Avatar 
-            image='https://asset.tokstudy.com/img_penguin.png'
-            userName='윤두현'
-            size='large'/>
-            <Avatar 
-            image='https://asset.tokstudy.com/img_penguin.png'
-            userName='윤두현'
-            size='large'/>
-            <Avatar 
-            image='https://asset.tokstudy.com/img_penguin.png'
-            userName='윤두현'
-            size='large'/>
-            <Avatar 
-            image='https://asset.tokstudy.com/img_penguin.png'
-            userName='윤두현'
-            size='large'/>
-            <Avatar 
-            image='https://asset.tokstudy.com/img_penguin.png'
-            userName='윤두현'
-            size='large'/>
-            <Avatar 
-            image='https://asset.tokstudy.com/img_penguin.png'
-            userName='윤두현'
-            size='large'/>
-            <Avatar 
-            image='https://asset.tokstudy.com/img_penguin.png'
-            userName='윤두현'
-            size='large'/>
-            <Avatar.Label
-            label='+2'
-            userNames={['윤두현', '현두윤']}
-            size='large'/>
-          </Avatar.Group>
         </Wrapper>
       </Section>
     </Page>
--- shared/toks-components/src/components/Avatar/index.tsx
+++ shared/toks-components/src/components/Avatar/index.tsx
@@ -1,12 +1,12 @@
-import styled from '@emotion/styled';
 import { theme } from '@depromeet/theme';
+import styled from '@emotion/styled';
 import { Avatar as BaseAvatar } from 'primereact/avatar';
 import { AvatarGroup } from 'primereact/avatargroup';
-import { ComponentProps, ReactNode } from 'react';
+import { ComponentProps } from 'react';
 
 type AvatarSize = 'normal' | 'large' | 'xlarge';
 
-interface AvatarProps extends Omit<ComponentProps<typeof BaseAvatar>, 'image' | 'size'> {
+export interface AvatarProps extends Omit<ComponentProps<typeof BaseAvatar>, 'image' | 'size'> {
   image: string;
   userName: string;
   size?: AvatarSize;
@@ -21,58 +21,76 @@ interface LabelAvatarProps extends Omit<ComponentProps<typeof BaseAvatar>, 'labe
 const AVATAR_SIZE = {
   normal: '22px',
   large: '32px',
-  xlarge: '40px'
+  xlarge: '40px',
 };
 
-export function Avatar({image, userName, size="normal"} : AvatarProps) {
+// TODO : 디폴트 이미지가 직접 넘어오는지 빈 값으로 넘어오는지에 따라 디폴트 프로필 이미지 출력을 구현해야함
+export function Avatar({ image, userName, size = 'normal' }: AvatarProps) {
   userName; // tooltip 때문에 일단 받아놓기 위한....일단 사용은 해야해서...
   return (
-    <StyledAvatar 
-      image={image} 
+    <StyledAvatar
+      image={image}
       size={size}
       shape="circle"
       // TODO: inline style로 적용한 부분 제외하기...^^ (현구님 따라해서 일단 저도 이렇게 했슴다..)
       style={{
         width: AVATAR_SIZE[size],
         height: AVATAR_SIZE[size],
-        border: `1px solid ${theme.colors.gray100}`
+        border: `1px solid ${theme.colors.gray100}`,
       }}
-    >
-    </StyledAvatar>
+    ></StyledAvatar>
   );
 }
 
 const StyledAvatar = styled(BaseAvatar)`
-   .p-avatar-text {
-     font-size: 14px;
-   }
+  .p-avatar-text {
+    font-size: 14px;
+  }
 `;
 
-interface RowProps{
-  children: ReactNode;
+const makeAvatars = (avatarInfos: AvatarProps[]) =>
+  avatarInfos.map(avatarInfo => (
+    <Avatar image={avatarInfo.image} userName={avatarInfo.userName} size={avatarInfo.size} />
+  ));
+
+const makeAvatarLabel = (avatarInfos: AvatarProps[]) =>
+  avatarInfos.length !== 0 ? (
+    <Avatar.Label
+      label={`+${avatarInfos.length}`}
+      userNames={avatarInfos.map(avartarInfo => avartarInfo.userName)}
+      size={'large'}
+    />
+  ) : null;
+
+const makeAvatarGroup = (avatarInfos: AvatarProps[]) => [
+  ...makeAvatars(avatarInfos.slice(0, 6)),
+  makeAvatarLabel(avatarInfos.slice(6)),
+];
+
+interface RowProps {
+  avatarInfos: AvatarProps[];
 }
 
 // TODO: maxView 개수 받도록
-function Group({ children }: RowProps) {
-  return <AvatarGroup>{children}</AvatarGroup>;
+function Group({ avatarInfos }: RowProps) {
+  return <AvatarGroup>{makeAvatarGroup(avatarInfos)}</AvatarGroup>;
 }
 
 // TODO: Avatar와 중복되는 요소들을 어떻게 합칠지 고민해봐야 함
-function Label({label, userNames, size="normal"} : LabelAvatarProps) {
+function Label({ label, userNames, size = 'normal' }: LabelAvatarProps) {
   userNames;
   return (
-    <StyledAvatar 
-      label={label} 
+    <StyledAvatar
+      label={label}
       size={size}
       shape="circle"
       style={{
         width: AVATAR_SIZE[size],
         height: AVATAR_SIZE[size],
         backgroundColor: `${theme.colors.gray060}`,
-        border: `1px solid ${theme.colors.gray100}`
+        border: `1px solid ${theme.colors.gray100}`,
       }}
-    >
-    </StyledAvatar>
+    ></StyledAvatar>
   );
 }
 
