7373500308c9d5689ab39c3ab38ea14680a3a8a1
chaaerim <cofla411@ewhain.net>
2022-11-26 22:24:07 +0900

    :recycle: refactor: 현구님의 가르침 반영

---
+++ services/home/src/JoinStudy/components/JoinStudyBox/index.tsx
@@ -0,0 +1,83 @@
+import { Button, Image, Tag, Text } from '@depromeet/toks-components';
+import { Flex, Spacing, width100 } from '@toss/emotion-utils';
+
+import { StudyInfo } from 'JoinStudy/components/StudyInfo';
+import { StudyTitle } from 'JoinStudy/components/StudyTitle';
+
+import { Wrapper } from './style';
+
+export function JoinStudyBox() {
+  // mock data
+  const ourStudyDescription =
+    '설명은 100자 제한입니다 설명은 100자 제한입니다 설명은 100자 제한입니다 설명은 100자 제한입니다 설명은 100자 제한입니다 설명은 100자 제한입니다 설명은 100자 제한입니다 설명은 100자 제한입니다 설명은 100자 제한입니다 설명은 100자 제한';
+  const personnelDescription = '5-7명을 계획하고 있어요.';
+  const startDate = '2022. 10. 13';
+  const doneDate = '2022. 12. 03';
+  const tags = ['Java', 'Javascript', 'React'];
+  return (
+    <Wrapper>
+      <div>
+        <StudyTitle />
+        {/* tag margin 위아래 10 고려하여 18->8 */}
+        <Spacing size={8} />
+        <Tag.Row>
+          {tags?.map(tag => (
+            <Tag key={tag}>{tag}</Tag>
+          ))}
+        </Tag.Row>
+      </div>
+      <Flex direction="column" css={{ gap: '32px' }}>
+        <StudyInfo
+          leftAddon={
+            <Image
+              width={16}
+              height={22}
+              src="https://toks-web-assets.s3.amazonaws.com/studyinfo-icon.svg"
+              alt="StudyInfo 아이콘"
+            />
+          }
+          title={'우리 스터디는'}
+          description={<Text variant="body01">{ourStudyDescription}</Text>}
+        />
+        <StudyInfo
+          leftAddon={
+            <Image
+              width={16}
+              height={22}
+              src="https://toks-web-assets.s3.amazonaws.com/studyinfo-icon.svg"
+              alt="StudyInfo 아이콘"
+            />
+          }
+          title={'스터디 기간은'}
+          description={
+            <>
+              <Text variant="body02">시작일</Text>
+              <Text variant="body01" css={{ marginLeft: '12px' }}>
+                {startDate}
+              </Text>
+              <Text variant="body02" css={{ marginLeft: '36px' }}>
+                종료일
+              </Text>
+              <Text variant="body01" css={{ marginLeft: '12px' }}>
+                {doneDate}
+              </Text>
+            </>
+          }
+        />
+        <StudyInfo
+          leftAddon={
+            <Image
+              width={16}
+              height={22}
+              src="https://toks-web-assets.s3.amazonaws.com/studyinfo-icon.svg"
+              alt="StudyInfo 아이콘"
+            />
+          }
+          title={'스터디 인원은'}
+          description={<Text variant="body01">{personnelDescription}</Text>}
+        />
+      </Flex>
+      <Button css={width100}>참여하기</Button>
+    </Wrapper>
+  );
+}
--- services/home/src/JoinStudy/components/StudyInfo/index.tsx
+++ services/home/src/JoinStudy/components/StudyInfo/index.tsx
@@ -1,7 +1,6 @@
 import { Text } from '@depromeet/toks-components';
+import { Flex, Spacing } from '@toss/emotion-utils';
 import { ReactNode } from 'react';
-
-import { DetailDescriptionText, StudyTitleStyle, TitleWrapper } from './style';
 
 // 컴포넌트를 받을 때 ReactElement로 props를 받는게 맞는지 확인 필요.
 // 공통 컴포넌트 개발이후에 스타일 일부 제거 필요.
@@ -12,14 +11,13 @@ type StudyInfoProps = {
 };
 export function StudyInfo({ leftAddon, title, description }: StudyInfoProps) {
   return (
-    <>
-      <TitleWrapper>
+    <div>
+      <Flex css={{ gap: '11px' }}>
         {leftAddon}
-        <Text size={14} weight={700} css={StudyTitleStyle}>
-          {title}
-        </Text>
-      </TitleWrapper>
-      <DetailDescriptionText>{description}</DetailDescriptionText>
-    </>
+        <Text variant="subhead">{title}</Text>
+      </Flex>
+      <Spacing size={12} />
+      <Text>{description}</Text>
+    </div>
   );
 }
