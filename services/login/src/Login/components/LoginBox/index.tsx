1a0fb3fa59fa302cc90b90a0c346078ff354b403
chaaerim <cofla411@ewhain.net>
2022-11-29 04:55:55 +0900

    :sparkles: feat: 로그인 페이지 임시 개발

---
+++ services/login/src/Login/components/LoginBox/index.tsx
@@ -0,0 +1,22 @@
+import { Button, Image } from '@depromeet/toks-components';
+import { Spacing } from '@toss/emotion-utils';
+import { Wrapper } from './style';
+import { Text } from '@depromeet/toks-components';
+import { useRouter } from 'next/router';
+
+export function LoginBox() {
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push('https://tokstudy.com/oauth2/authorize/kakao');
+  };
+  return (
+    <Wrapper>
+      <Image src="https://asset.tokstudy.com/awake-yellow-emoji.png" width={100} height={100} alt="toks-emoji" />
+      <Spacing size={32} />
+      <Text variant="title04">개발자를 위한 스터디, 똑스-잇!</Text>
+      <Spacing size={93} />
+      <Button onClick={onClick}>Kakao 로그인</Button>
+    </Wrapper>
+  );
+}
