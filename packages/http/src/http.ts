42e1753282a3515a69b85a703adabc67a1038068
chaaerim <cofla411@ewhain.net>
2022-11-30 04:42:03 +0900

    :sparkles: feat: 쿼리에서 access, refresh 받아오기

--- packages/http/src/http.ts
+++ packages/http/src/http.ts
@@ -1,22 +1,42 @@
 import axios from 'axios';
 
 let authToken = {
-  //TODO: 액세스토큰 백에서 받아오는 작업 처리 필요
-  access: 'token',
+  access: sessionStorage.getItem('accessToken'),
+  refresh: sessionStorage.getItem('refreshToken'),
 };
 
+if (!authToken?.access || authToken?.access === '') {
+  //쿼리에서 액세스 토큰 리프레쉬 토큰 가져오기
+  //가져오고 accessToken이 null이 아니면 nickname페이지로 리다이렉트
+  const params = new URLSearchParams(location.search);
+  let accessToken = params.get('accssToken') || '';
+  let refreshToken = params.get('refreshToken') || '';
+  sessionStorage.setItem('accessToken', accessToken);
+  sessionStorage.setItem('refreshToken', refreshToken);
+  if (authToken?.access) {
+    history.pushState('', '', 'https://tokstudy.com/login/myName');
+  }
+} else if (authToken?.access) {
+  // refresh 토큰 보내서 만료되었는지 확인?
+  // 응답이 refresh 만료면 다시 로그인 페이지로
+}
+
+//axios instance
 const instance = axios.create({
   baseURL: 'https://api.tokstudy.com',
-  headers: { Authorizezation: `Bearer ${authToken?.access}` },
+  headers: { Authorization: `Bearer ${authToken?.access}` },
 });
-// authprovider를 만들어야하나?
-//1. 요청 인터셉터
 
+//1. 요청 인터셉터
 instance.interceptors.request.use(
   function (config) {
-    //요청 보내기 전 수행할 일, 요청 성공 직전 호출
-    // config.headers['Content-Type'] = 'application/json; charset=utf-8';
-    // config.headers['Authorization'] = ' 토큰 값';
+    //@ts-ignore
+    config.headers['Content-Type'] = 'application/json; charset=utf-8';
+    //@ts-ignore
+    if (!config.headers['Authorization']) {
+      //@ts-ignore
+      config.headers['Authorization'] = `${authToken?.access}`;
+    }
     return config;
   },
   function (error) {
@@ -25,24 +45,11 @@ instance.interceptors.request.use(
 );
 
 //2. 응답 인터셉터
-instance.interceptors.response.use(
-  function (response) {
-    /*
-        http status가 200인 경우
-        응답 성공 직전 호출됩니다. 
-        .then() 으로 이어집니다.
-    */
-
-    return response;
-  },
-  function (error) {
-    /*
-            http status가 200이 아닌 경우
-            응답 에러 직전 호출됩니다.
-            .catch() 으로 이어집니다.    
+instance.interceptors.response.use(function (error) {
+  /*
+    401에러인경우 
         */
-    return Promise.reject(error);
-  }
-);
+  return Promise.reject(error);
+});
 
 export default instance;
