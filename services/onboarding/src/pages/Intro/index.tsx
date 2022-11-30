813f29a43c9265a828c76c27745ef7ced98524b2
minsgy <alstjrchl97@likelion.org>
2022-12-01 00:38:59 +0900

    ✨ feat: onboarding intro 페이지 구현

--- services/onboarding/src/pages/Intro/index.tsx
+++ services/onboarding/src/pages/Intro/index.tsx
@@ -1,15 +1,16 @@
-import { Image } from '@depromeet/toks-components';
+import { ToksHeader } from '@depromeet/toks-components';
+import { Banner } from './components/Banner';
+import { Wrapper } from './style';
 
 function Intro() {
   return (
-    <div
-      style={{
-        width: '200px',
-        height: '200px',
-      }}
-    >
-      <Image src="https://picsum.photos/200/300" mode="cover" alt="테스트" />
-    </div>
+    <>
+      {/* TODO: 해당 부분 Fixed 변경 */}
+      <ToksHeader />
+      <Wrapper>
+        <Banner />
+      </Wrapper>
+    </>
   );
 }
 
