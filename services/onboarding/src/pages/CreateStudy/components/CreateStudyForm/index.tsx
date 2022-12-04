9d687aef0a2fed4b5f68ed51ccad3eb15a34a68e
minsgy <alstjrchl97@likelion.org>
2022-12-04 13:36:30 +0900

    ✨ feat: 스터디 생성 InputChips 적용

--- services/onboarding/src/pages/CreateStudy/components/CreateStudyForm/index.tsx
+++ services/onboarding/src/pages/CreateStudy/components/CreateStudyForm/index.tsx
@@ -1,33 +1,59 @@
-import { Button, Calendar, Input, Text } from '@depromeet/toks-components';
+import { Button, Calendar, InputChips, Input, Text } from '@depromeet/toks-components';
 import { Flex, gutter, margin, size } from '@toss/emotion-utils';
+import { useState } from 'react';
 
+import { useForm } from 'react-hook-form';
 import { Wrapper } from './style';
 
 export const CreateStudyForm = () => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  };
+  const { register, handleSubmit, getValues } = useForm();
+  const [value, setValue] = useState<string[]>([]);
 
   return (
     <Wrapper>
       <Text as="h2" variant="title03" css={{ textAlign: 'center' }}>
         똑스터디 만들기
       </Text>
-      <form onSubmit={handleSubmit} css={(margin.top(42), size({ width: '100%' }), gutter('vertical', 32))}>
-        <Input label="스터디 이름*" name="studyName" placeholder="스터디 이름을 입력해주세요. (20자 이내)" autoFocus />
+      <form
+        onSubmit={handleSubmit(data => alert(JSON.stringify(data)))}
+        css={(margin.top(42), size({ width: '100%' }), gutter('vertical', 32))}
+      >
+        <Input
+          label="스터디 이름*"
+          placeholder="스터디 이름을 입력해주세요. (20자 이내)"
+          autoFocus
+          {...register('studyName', { required: true })}
+        />
         <Input
           label="스터디 설명"
-          name="studyDescription"
           placeholder="스터디 목표나 간단한 소개를 작성해주세요. (50자 이내)"
           errorMessage="스터디 설명을 입력해주세요."
+          {...register('studyDescription')}
         />
         <Flex css={gutter('horizontal', 24)}>
-          <Calendar readOnlyInput label="스터디 시작일*" minDate={new Date()} placeholder="날짜 선택" />
-          <Calendar readOnlyInput label="스터디 종료일*" minDate={new Date()} placeholder="날짜 선택" />
+          <Calendar
+            readOnlyInput
+            label="스터디 시작일*"
+            minDate={new Date()}
+            placeholder="날짜 선택"
+            {...register('startDate', { required: true })}
+          />
+          <Calendar
+            readOnlyInput
+            label="스터디 종료일*"
+            minDate={new Date()}
+            placeholder="날짜 선택"
+            {...register('endDate', { required: true })}
+          />
         </Flex>
-        <Input label="스터디 인원*" name="memberCount" />
-        <Input label="스터디 관련 태그" name="category" />
-        <Button type="primary" htmlType="submit" buttonStyle={{ width: '100%' }}>
+        <Input label="스터디 인원*" {...register('memberCount', { required: true })} />
+        <InputChips
+          value={getValues('tags')}
+          name="studyTags"
+          onChange={e => setValue(e.value)}
+          label="스터디 관련 태그"
+        />
+        <Button type="primary" htmlType="submit">
           생성하기
         </Button>
       </form>
