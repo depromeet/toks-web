1f15241c919cea6dedcf2c1d7b8878e33b229a11
chaaerim <cofla411@ewhain.net>
2022-12-05 17:11:24 +0900

    :recycle: refactor: LoginBox Auth code 삭제

--- services/login/src/Login/components/LoginBox/index.tsx
+++ services/login/src/Login/components/LoginBox/index.tsx
@@ -4,35 +4,10 @@ import { useRouter } from 'next/router';
 
 import { Wrapper } from '../../../common/style';
 
-export function Auth() {
-  const params = new URLSearchParams(location.search);
-  const authToken = {
-    access: sessionStorage.getItem('accessToken'),
-    refresh: sessionStorage.getItem('refreshToken'),
-  };
-  if (!authToken?.access) {
-    // let { accessToken, refreshToken } = URLSearchParams<{ accessToken: string; refreshToken: string }>();
-    const accessToken = params.get('accessToken');
-    const refreshToken = params.get('refreshToken');
-    console.log(accessToken);
-    console.log(refreshToken);
-    console.log(params);
-
-    if (accessToken && refreshToken) {
-      sessionStorage.setItem('accessToken', accessToken);
-      sessionStorage.setItem('refreshToken', refreshToken);
-    }
-  }
-}
 export function LoginBox() {
   const router = useRouter();
   const onClick = () => {
     router.push('https://api.tokstudy.com/oauth2/authorize/kakao');
-
-    setTimeout(() => {
-      Auth();
-      console.log('hihihi');
-    }, 100);
   };
 
   return (
