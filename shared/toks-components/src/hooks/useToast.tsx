173e7341bfc60539d57ca7bd8a8c6a05f4fb3387
goo <goo@tossbank.com>
2022-12-07 19:18:19 +0900

    feat: 토스트 개발 - useToast

---
+++ shared/toks-components/src/hooks/useToast.tsx
@@ -0,0 +1,42 @@
+import { useOverlay } from '@toss/use-overlay';
+import { ComponentProps } from 'react';
+
+import { Toast } from '../components/Toast';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const TOAST_OPENED_TIME = 3_000;
+
+interface Props extends ComponentProps<typeof Toast> {
+  time?: number;
+}
+
+export function useToast() {
+  const overlay = useOverlay();
+
+  const openToast = ({ time = TOAST_OPENED_TIME, ...rest }: Props) =>
+    new Promise(resolve => {
+      overlay.open(({ isOpen, close }) => {
+        window.setTimeout(close, time);
+
+        return (
+          <AnimatePresence>
+            {isOpen ? (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.15 }}
+                onAnimationStart={resolve}
+              >
+                <Toast {...rest} />
+              </motion.div>
+            ) : null}
+          </AnimatePresence>
+        );
+      });
+    });
+
+  return { open: openToast, close: overlay.close };
+}
+
+export default useToast;
