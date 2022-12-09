52603812d66b451797f7fe9d2113b4a72af4636e
minsgy <alstjrchl97@likelion.org>
2022-12-09 18:13:35 +0900

    🔥 remove: maxChips 제한 삭제

--- services/onboarding/src/pages/CreateStudy/components/CreateStudyForm/index.tsx
+++ services/onboarding/src/pages/CreateStudy/components/CreateStudyForm/index.tsx
@@ -72,7 +72,7 @@ export const CreateStudyForm = () => {
           name="studyTags"
           control={control}
           render={({ field }) => (
-            <InputChips {...field} onChange={e => setValue('studyTags', e.value)} label="스터디 관련 태그" max={5} />
+            <InputChips {...field} onChange={e => setValue('studyTags', e.value)} label="스터디 관련 태그" />
           )}
         />
         <Button type="primary" htmlType="submit" disabled={isDisabled}>
