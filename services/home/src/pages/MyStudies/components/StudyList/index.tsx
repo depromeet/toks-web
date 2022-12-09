0f98d4d7c8f8bf7d500ef146af5ea6258c659b4c
HYUNGU KANG <69349293+LineGu@users.noreply.github.com>
2022-12-09 21:50:48 +0900

    feat(home, @depromeet/path, @depromeet/toks-components): my study 페이지 마무리 / TextBallon Component / routing util 개발 (#97)
    * feat: my study 페이지 마무리 / TextBallon Component / routing util 개발
    
    * refactor: lint
    
    * refactor: 리뷰 반영
    
    Co-authored-by: goo <goo@tossbank.com>

--- services/home/src/pages/MyStudies/components/StudyList/index.tsx
+++ services/home/src/pages/MyStudies/components/StudyList/index.tsx
@@ -1,50 +1,58 @@
-import { PATHS } from '@depromeet/path';
-import { SSRSuspense, useToast } from '@depromeet/toks-components';
+import { PATHS, pushTo } from '@depromeet/path';
+import { SSRSuspense } from '@depromeet/toks-components';
+import styled from '@emotion/styled';
 import { Flex } from '@toss/emotion-utils';
 import { ErrorBoundary } from '@toss/error-boundary';
 import { useSuspendedQuery } from '@toss/react-query';
-import { useRouter } from 'next/router';
 
 import { QUERY_KEYS } from 'constants/queryKeys';
 import { getMyStudies } from 'pages/MyStudies/remotes/study';
 
 import StudyCard from '../StudyCard';
 
+const MAX_STUDY_CNT = 4;
+
 function StudyList() {
   const { data: myStudies } = useSuspendedQuery(QUERY_KEYS.GET_MY_STUDIES, getMyStudies);
 
-  const router = useRouter();
-  const { open } = useToast();
-
   return (
-    <Flex css={{ gap: '32px', alignSelf: 'flex-start', width: '90%', margin: '0 auto' }} as="ul">
+    <StudyListRow as="ul">
       {myStudies.map(study => (
         <SSRSuspense fallback={<StudyCard.Skeleton />} key={study.id}>
           <StudyCard
             title={study.title}
             tags={study.tags}
-            onClick={async () => {
-              // 토스트 예시
-              await open({ title: '스터디를 눌렀습니다', type: 'success', direction: ['left', 'top'], time: 10000 });
-              await open({ title: '스터디를 눌렀습니다2', type: 'danger', direction: 'bottom' });
-
-              await router.push(PATHS.quiz.studyDetail({ studyId: study.id }));
-            }}
             memberCount={study.member.length}
             studyId={study.id}
+            onClick={() => pushTo(PATHS.quiz.studyDetail({ studyId: study.id }))}
           />
         </SSRSuspense>
       ))}
-      {/* TODO: 스터디 생성 로직 구현 */}
-      {myStudies.length < 4 ? <StudyCard.Plus onClick={() => {}} /> : null}
-    </Flex>
+
+      {myStudies.length < MAX_STUDY_CNT && <StudyCard.Plus onClick={() => pushTo(PATHS.onboarding.createStudy)} />}
+    </StudyListRow>
   );
 }
+
+const StudyListRow = styled(Flex)`
+  gap: 32px;
+  align-self: flex-start;
+  width: min(100%, 1260px);
+  margin: 0 auto;
+  padding: 100px 20px;
+  overflow-x: auto;
+`;
 
 export default () => (
   // TODO: fallback 개선
   <ErrorBoundary renderFallback={() => null}>
-    <SSRSuspense fallback={null}>
+    <SSRSuspense
+      fallback={
+        <StudyListRow css={{ visibility: 'hidden' }}>
+          <StudyCard.Skeleton />
+        </StudyListRow>
+      }
+    >
       <StudyList />
     </SSRSuspense>
   </ErrorBoundary>
