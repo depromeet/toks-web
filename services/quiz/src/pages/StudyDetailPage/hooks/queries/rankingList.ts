b66d6f8898d11438d368beaa2c45bd84833c7152
dengoyoon <engus927@naver.com>
2022-12-14 07:32:11 +0900

    🐛 fix: 랭킹 아이템 인터페이스 이름 바꾼거 마저 반영

--- services/quiz/src/pages/StudyDetailPage/hooks/queries/rankingList.ts
+++ services/quiz/src/pages/StudyDetailPage/hooks/queries/rankingList.ts
@@ -1,9 +1,9 @@
 import { useSuspendedQuery } from '@toss/react-query';
 
 import { QUERY_KEYS } from 'constants/queryKeys';
-import { RankingItem } from 'pages/StudyDetailPage/models/rankingList';
+import { RankingItemResponse } from 'pages/StudyDetailPage/models/rankingList';
 import { getRankingList } from 'pages/StudyDetailPage/remotes/rankingList';
 
 export const useGetRankingList = () => {
-  return useSuspendedQuery<RankingItem[]>(QUERY_KEYS.GET_RANKING_LIST, getRankingList);
+  return useSuspendedQuery<RankingItemResponse[]>(QUERY_KEYS.GET_RANKING_LIST, getRankingList);
 };
