39204a55b7baa9995b44b0499af585924d8f477d
chaaerim <cofla411@ewhain.net>
2022-12-14 16:38:53 +0900

    :bug: fix: yarn install

--- .pnp.cjs
+++ .pnp.cjs
@@ -5981,6 +5981,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@configs/next", "virtual:bf385740268fd646fb11939e2de4db770daf461f5380f1f23f2a193c453ad695c7fa46bf5f9fd706cfde63c5461e698d99c5ae46eacb0c996ed6a8d7fbf19e4d#workspace:configs/next"],\
             ["@depromeet/http", "npm:0.2.0"],\
             ["@depromeet/layout", "virtual:64da7d8dcebb3a60a500ffa0a91d19f724793a1c06213ebbc873cbea5b76a36ca3a67d9e3deed6a024ae864da32868cd970cb6b4b0d9f2c3d049f95a1a60a7aa#workspace:shared/layout"],\
+            ["@depromeet/path", "workspace:shared/path"],\
             ["@depromeet/theme", "npm:0.0.1"],\
             ["@depromeet/toks-components", "virtual:64da7d8dcebb3a60a500ffa0a91d19f724793a1c06213ebbc873cbea5b76a36ca3a67d9e3deed6a024ae864da32868cd970cb6b4b0d9f2c3d049f95a1a60a7aa#workspace:shared/toks-components"],\
             ["@emotion/css", "virtual:8f1d23321fc73f63a23a69eea049bd4e118ed160511334e71a15d0291d2a970ebd766ecc894350935b562df93b206d16a7edc9e1abfbeb41b996b404ab9a67c3#npm:11.10.0"],\
