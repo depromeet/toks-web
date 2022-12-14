d56c744820b606578e825ffcc9cf2fb79c255484
goo <goo@tossbank.com>
2022-12-14 17:09:14 +0900

    feat: http 수정

--- packages/http/src/http.ts
+++ packages/http/src/http.ts
@@ -25,12 +25,10 @@ const authToken = {
     typeof window === 'undefined' && typeof global !== 'undefined' ? null : sessionStorage.getItem('refreshToken'),
 };
 
-export function Auth() {
-  // 엑세스 토큰이 없는 경우 로그인 페이지로 라우팅
-  if (authToken.access == null) {
-    window.location.href = 'https://api.tokstudy.com/oauth2/authorize/kakao';
-  }
-}
+const redirectToLoginPage = () => {
+  const isDev = window.location.hostname === 'localhost';
+  window.location.href = isDev ? 'http://localhost:3000/login' : 'https://tokstudy.com/login';
+};
 
 //axios instance
 const instance: ToksHttpClient = axios.create({
@@ -59,27 +57,28 @@ instance.interceptors.response.use(
   response => response.data,
   async function (error) {
     if (error?.status === 401 && error?.data?.code === -20027) {
-      //액세스 토큰이 invalid 한 경우 refresh 확인
-      const res = await axios.post('/api/v1/user/renew', authToken.refresh);
-      if (res.status === 201) {
+      try {
+        const {
+          data: { refreshToken },
+        } = await axios.post('/api/v1/user/renew', authToken.refresh);
         //refresh 유효한 경우 새롭게 accesstoken 설정
-        const newAccessToken = res.data;
+
         if (error?.config.headers === undefined) {
           error.config.headers = {};
         } else {
-          error.config.headers['Authorization'] = newAccessToken;
+          error.config.headers['Authorization'] = refreshToken;
           //sessionStorage에 새 토큰 저장
-          sessionStorage.setItem('accessToken', newAccessToken);
+          sessionStorage.setItem('accessToken', refreshToken);
           // 중단된 요청 새로운 토큰으로 재전송
           const originalResponse = await axios.request(error.config);
           return originalResponse.data.data;
         }
-      } else if (res.data.code === -20014) {
-        // refresh invalid한 경우 다시 로그인
-        window.location.href = 'https://api.tokstudy.com/oauth2/authorize/kakao';
+      } catch (err) {
+        redirectToLoginPage();
       }
+    } else {
+      throw error;
     }
-    return Promise.reject(error);
   }
 );
 
