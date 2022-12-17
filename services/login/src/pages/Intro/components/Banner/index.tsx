220c398fbb514da9cb90c12b1cd057b981d01073
chaaerim <cofla411@ewhain.net>
2022-12-17 19:00:02 +0900

    :bug: fix: 코드 원상복귀

--- services/login/src/pages/Intro/components/Banner/index.tsx
+++ services/login/src/pages/Intro/components/Banner/index.tsx
@@ -2,7 +2,7 @@ import { Button, Text } from '@depromeet/toks-components';
 import { Flex } from '@toss/emotion-utils';
 import { useMutation } from 'react-query';
 
-import { login as requestLogin } from '../../remote/login';
+import { login as requestLogin } from 'pages/Intro/remote/login';
 
 export const Banner = () => {
   const { mutateAsync: login, isLoading } = useMutation(requestLogin);
@@ -20,6 +20,7 @@ export const Banner = () => {
       <Text variant="title01" color="white">
         개발자를 위한 스터디, 똑스-잇!
       </Text>
+
       <Button type="general" width={200} size="large" onClick={() => login()} loading={isLoading}>
         똑스 로그인
       </Button>
