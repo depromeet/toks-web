a3d93168caf992235c5ec98373703b4bd76c8d8c
chaaerim <cofla411@ewhain.net>
2022-12-20 05:29:33 +0900

    :sparkles: feat: useModal hook 생성

--- shared/toks-components/src/components/Modal/index.tsx
+++ shared/toks-components/src/components/Modal/index.tsx
@@ -2,38 +2,18 @@ import { theme } from '@depromeet/theme';
 import styled from '@emotion/styled';
 import { ReactNode } from 'react';
 
-import { useCloaseModal } from '../../hooks';
-
 interface Props {
   children: ReactNode;
-  onClose: () => void;
 }
 
-export function Modal({ children, onClose }: Props) {
-  const outSide = useCloaseModal(() => {
-    onClose();
-  });
-  return (
-    <ModalWrapper ref={outSide}>
-      <ModalContainer>{children}</ModalContainer>
-    </ModalWrapper>
-  );
+export function Modal({ children }: Props) {
+  return <ModalContainer>{children}</ModalContainer>;
 }
-
-const ModalWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0%;
-  background-color: rgba(0, 0, 0, 0.5);
-  z-index: 1010;
-`;
 
 const ModalContainer = styled.div`
   position: relative;
   vertical-align: auto;
+  text-align: center;
   padding: 28px;
   width: 600px;
   background-color: ${theme.colors.gray110};
--- shared/toks-components/src/hooks/index.ts
+++ shared/toks-components/src/hooks/index.ts
@@ -1,3 +1,3 @@
 export * from './useToast';
 export * from './useClipboard';
-export * from './useCloseModal';
+export * from './useModal';
