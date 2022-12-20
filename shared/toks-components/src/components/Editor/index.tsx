5b2f0bf61d135e297141dcb8dff62d056122da59
minsgy <alstjrchl97@likelion.org>
2022-12-20 20:53:34 +0900

    refactor: Dropdown ref 연결 및 에디터 네이밍 변경

--- shared/toks-components/src/components/Editor/index.tsx
+++ shared/toks-components/src/components/Editor/index.tsx
@@ -1,16 +1,16 @@
 import { isServer } from '@toss/utils';
 import React, { ComponentProps, Suspense } from 'react';
 
-const Editor = React.lazy(() => import('./Editor'));
+const EditorComponent = React.lazy(() => import('./Editor'));
 
-export function SSRSafeEditor(props: ComponentProps<typeof Editor>) {
+export function Editor(props: ComponentProps<typeof EditorComponent>) {
   if (isServer()) {
     return null;
   }
 
   return (
     <Suspense fallback={null}>
-      <Editor {...props} />
+      <EditorComponent {...props} />
     </Suspense>
   );
 }
