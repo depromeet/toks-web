c44f5b4ec327016ef328fc0e13adcff08c44f391
minsgy <alstjrchl97@likelion.org>
2022-12-21 00:20:31 +0900

    🔥 remove: onboarding/remote getStudyForm remote 함수 삭제

--- services/onboarding/src/pages/CreateStudy/remotes/study.ts
+++ services/onboarding/src/pages/CreateStudy/remotes/study.ts
@@ -6,7 +6,3 @@ import { CreateStudyFormValues } from '../components/CreateStudyForm/type';
 export async function postStudy(study: CreateStudyFormValues) {
   return await http.post<StudyResponse>('/api/v1/study', study);
 }
-
-export async function getStudyFormData(studyId: string | number) {
-  return await http.get<StudyResponse>(`/api/v1/study/${studyId}`);
-}
