e0d6a40b9a6d24b43ff667df245c5a8cc8699a09
goo <goo@tossbank.com>
2022-12-21 19:22:34 +0900

    refactor: 예시로 넣은 컴포넌트 삭제

--- services/login/src/pages/Intro/index.tsx
+++ services/login/src/pages/Intro/index.tsx
@@ -1,11 +1,9 @@
-import { Timer } from '@depromeet/toks-components';
 import { Banner } from './components/Banner';
 import { Wrapper } from './style';
 
 function Intro() {
   return (
     <Wrapper>
-      <Timer />
       <Banner />
     </Wrapper>
   );
