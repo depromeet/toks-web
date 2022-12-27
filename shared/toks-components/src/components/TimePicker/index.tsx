00121b9074de1a9863f17662762e34d193ce9060
dengoyoon <engus927@naver.com>
2022-12-27 18:14:36 +0900

    ✨ feat: 타임피커에 상태값 부여하여 결과값 도출 구현

--- shared/toks-components/src/components/TimePicker/index.tsx
+++ shared/toks-components/src/components/TimePicker/index.tsx
@@ -1,54 +1,104 @@
-import { HTMLAttributes, useState } from 'react';
+import { HTMLAttributes, useState, Dispatch, SetStateAction } from 'react';
 import { theme } from '@depromeet/theme';
 import styled from '@emotion/styled';
 import { Input, Text } from '@depromeet/toks-components';
 
+type AMPM = "AM" | "PM";
+
 interface TimePickerProps extends HTMLAttributes<HTMLDivElement> {
-  hour? : number,
-  minute? : number,
-  isAM? : boolean,
+  defaultHour? : number,
+  defaultMinute? : number,
+  defaultAmpm? : AMPM
 }
 
 interface ToggleSwitchButtonProps extends HTMLAttributes<HTMLDivElement> {
-  initIsAM : boolean,
+  ampm : AMPM,
+  setAmpm: Dispatch<SetStateAction<AMPM>>;
 }
 
-const padZero = (value: string | number = 0) => value.toString().padStart(2, '0');
+const padZero = (value: number | string = 0) => value.toString().padStart(2, '0');
 
-function ToggleSwitchButton({ initIsAM } : ToggleSwitchButtonProps) {
-  const [isAM, setIsAM] = useState(initIsAM);
-
+function ToggleSwitchButton({ ampm, setAmpm } : ToggleSwitchButtonProps) {
   const getActiveColor = (isActive : boolean) => isActive? theme.colors.primary : theme.colors.gray090;
+  const isAM = ampm === "AM";
 
   return (
     <FlexRow style={{paddingTop: '6px', borderRadius: '6px'}}>
       <LeftToggleButton 
-        style={{
-          backgroundColor: `${getActiveColor(isAM)}`
-        }}
-        onClick={() => setIsAM(true)}>
+        style={{ backgroundColor: `${getActiveColor(isAM)}` }}
+        onClick={() => setAmpm("AM")}>
         <Text variant="body02">AM</Text>
       </LeftToggleButton>
       <RightToggleButton 
-        style={{
-          backgroundColor: `${getActiveColor(!isAM)}`
-        }}
-        onClick={() => setIsAM(false)}>
+        style={{ backgroundColor: `${getActiveColor(!isAM)}` }}
+        onClick={() => setAmpm("PM")}>
         <Text variant="body02">PM</Text>
       </RightToggleButton>
     </FlexRow>
   )
 }
 
-export function TimePicker({hour = 0, minute = 0, isAM = true} : TimePickerProps) {
+const convertTimeFormat = (hour : string, minute : string, ampm : AMPM) => {
+  return (ampm === "AM")? 
+    `${padZero(hour)}:${padZero(minute)}:00` :
+    `${padZero(Number(hour) + 12)}:${padZero(minute)}:00`
+}
+
+const useTimePicker = (defaultHour : number, defaultMinute : number, defaultAmpm : AMPM) => {
+  const [timePick, setTimePick] = useState({
+    hour : padZero(defaultHour),
+    minute : padZero(defaultMinute)
+  });
+  const { hour, minute } = timePick;
+  const [ampm, setAmpm] = useState(defaultAmpm);
+
+  const onUpdate = (e : React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+    setTimePick({
+      ...timePick, 
+      [name]: value
+    });
+  };
+
+  return {
+    hour,
+    minute,
+    ampm,
+    onUpdate,
+    setAmpm
+  }
+}
+
+export function TimePicker({defaultHour = 0, defaultMinute = 0, defaultAmpm = "AM"} : TimePickerProps) {
+  const {
+    hour,
+    minute,
+    ampm,
+    onUpdate,
+    setAmpm
+  } = useTimePicker(defaultHour, defaultMinute, defaultAmpm);
+
   return (
     <FlexRow>
+      <input 
+        type="hidden"
+        value={convertTimeFormat(hour, minute, ampm)}/>
       <FlexRow style={{marginRight: '20px'}}>
-        <Input label='' placeholder={padZero(hour)}/>
+        <Input 
+          label='' 
+          name='hour'
+          placeholder={padZero(defaultHour)}
+          autoComplete="off"
+          onChange={onUpdate}/>
         <Text variant="body01" style={{margin: '0 6px'}}>:</Text>
-        <Input label='' placeholder={padZero(minute)}/>
+        <Input 
+          label='' 
+          name='minute'
+          placeholder={padZero(defaultMinute)}
+          autoComplete="off"
+          onChange={onUpdate}/>
       </FlexRow>
-      <ToggleSwitchButton initIsAM={isAM}/>
+      <ToggleSwitchButton ampm={ampm} setAmpm={setAmpm}/>
     </FlexRow>
   );
 }
