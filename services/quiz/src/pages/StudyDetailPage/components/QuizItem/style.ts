a106e81123e980cd1419cb78bebe36db4919e8f4
dengoyoon <engus927@naver.com>
2022-12-31 01:45:52 +0900

    🔥 remove: 안쓰는 스타일 컴포넌트 지움

--- services/quiz/src/pages/StudyDetailPage/components/QuizItem/style.ts
+++ services/quiz/src/pages/StudyDetailPage/components/QuizItem/style.ts
@@ -6,21 +6,6 @@ export const ListItem = styled.li`
   }
 `;
 
-export const Item = styled.div`
-  padding: 22px 38.5px 22px 28px;
-  border-radius: 12px;
-`;
-
-export const ItemDetails = styled.details``;
-
-export const ItemHeader = styled.summary`
-  display: flex;
-  list-style: none;
-  align-items: center;
-`;
-
-export const ItemBody = styled.div``;
-
 export const FlexRow = styled.div`
   display: flex;
   align-items: center;
