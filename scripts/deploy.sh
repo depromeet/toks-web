1d437a465b6f1f505900355a92a8c48d15edd7c6
HYUNGU KANG <69349293+LineGu@users.noreply.github.com>
2023-01-01 15:55:17 +0900

    feat: 배포 스크립트 재설정 (#145)
    Co-authored-by: goo <goo@tossbank.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,12 +19,40 @@ echo "##################빌드 끝##################"
 
 echo "@@배포를 시작합니다@@"
 
-curl http://54.175.148.212:8080/init/${service}
+ip=""
+instance=""
+
+case $service in 
+    "login" ) 
+    ip="52.23.167.1"
+    instance="ec2-52-23-167-1.compute-1.amazonaws.com"
+    ;; 
+    "home" ) 
+    ip="3.87.30.27"
+    instance="ec2-3-87-30-27.compute-1.amazonaws.com"
+    ;; 
+    "quiz" ) 
+    ip="54.161.93.155"
+    instance="ec2-54-161-93-155.compute-1.amazonaws.com"
+    ;; 
+    "onboarding" ) 
+    ip="44.203.4.105"
+    instance="ec2-44-203-4-105.compute-1.amazonaws.com"
+    ;;
+    *)  
+    echo "일치하는 서비스가 없습니다." 
+    exit  
+    ;;
+esac
+
+echo "${ip}, ${instance}"
+
+curl http://${ip}:8080/init/${service}
 
 mkdir -p ./builds/
 
 tar -czv .next > ./builds/.next.tar.gz
 
-scp -r -i ../../../frontend-key.pem ./builds/.next.tar.gz ubuntu@ec2-54-175-148-212.compute-1.amazonaws.com:/home/ubuntu/builds/${service}/.next.tar.gz
+scp -r -i ../../../frontend-key.pem ./builds/.next.tar.gz ubuntu@${instance}:/home/ubuntu/builds/${service}/.next.tar.gz
 
-curl http://54.175.148.212:8080/deploy/${service}
+curl http://${ip}:8080/deploy/${service}
