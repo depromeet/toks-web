cdf4c1b13dfe2094ad981a4a0c9d89e095e11e35
dengoyoon <engus927@naver.com>
2023-01-02 18:45:02 +0900

    🚧 feat: api fetching 진행중

--- services/quiz/src/pages/StudyDetailPage/remotes/studyInfo.ts
+++ services/quiz/src/pages/StudyDetailPage/remotes/studyInfo.ts
@@ -5,3 +5,12 @@ import { StudyInfo } from '../models/studyInfo';
 export const getStudyInfo = () => {
   return new Promise<StudyInfo>(resolve => setTimeout(() => resolve(studyInfo), 800));
 };
+
+// export async function getStudyInfoById(studyId: string | string[] | undefined) {
+//   return await http.get<StudyInfo>(`/api/v1/studies/${studyId}`, {
+//     headers: {
+//       Authorization:
+//         'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwb29oOTYwNDA3QG5hdmVyLmNvbSIsImlhdCI6MTY3MTQ3MTMwMiwiZXhwIjoyMDQ0NzE5MzAyfQ.sp4-Y5XvsnMfNfVe1wbWE9xnTsNMJT8dR1QTAuNsM7A',
+//     },
+//   });
+// }
