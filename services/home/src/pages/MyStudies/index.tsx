36d27c89e06d623c85e37dc6a8afe1697c2ca123
goo <goo@tossbank.com>
2023-01-03 23:59:16 +0900

    feat: header 디자인 수정 및 layout 커스텀 가능하도록 변경

--- services/home/src/pages/MyStudies/index.tsx
+++ services/home/src/pages/MyStudies/index.tsx
@@ -1,16 +1,22 @@
-import { Text } from '@depromeet/toks-components';
+import { Layout } from '@depromeet/layout';
+import { Text, TOKS_HEADER_HEIGHT } from '@depromeet/toks-components';
 import { Flex } from '@toss/emotion-utils';
+import { ReactElement } from 'react';
 
 import StudyList from './components/StudyList';
 
-function MyStudis() {
+export default function MyStudies() {
   return (
-    <Flex.Center direction="column" justify="space-evenly" css={{ height: '90vh' }}>
-      <Text variant="title01">개발자를 위한 스터디, 똑스-잇!</Text>
+    <Flex.Center direction="column" justify="space-evenly" css={{ height: `calc(100vh - ${TOKS_HEADER_HEIGHT})` }}>
+      <Text variant="title01" css={{ textAlign: 'center' }}>
+        개발자를 위한 스터디, 똑스-잇!
+      </Text>
 
       <StudyList />
     </Flex.Center>
   );
 }
 
-export default MyStudis;
+MyStudies.getLayout = function getLayout(page: ReactElement) {
+  return <Layout fullWidth>{page}</Layout>;
+};
