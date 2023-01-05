e6cca54d1789c1d72a98532774754c058b1e7934
Dengo <engus927@naver.com>
2023-01-05 17:29:33 +0900

    feat(quiz): 프로그레스바 구현 (#162)
    * ✨ feat: 프로그레스바 구현
    
    * ✨ feat: api 값을 받아서 출력하도록 변경

--- services/quiz/src/pages/StudyDetailPage/components/StudyInfo/index.tsx
+++ services/quiz/src/pages/StudyDetailPage/components/StudyInfo/index.tsx
@@ -3,7 +3,8 @@ import { ErrorBoundary } from '@toss/error-boundary';
 
 import { useGetStudyInfo } from 'pages/StudyDetailPage/hooks/queries/studyInfo';
 
-import { Body, Footer, Header, Info, StudyTags } from './style';
+import { StudyProgress } from '../StudyProgress';
+import { Body, FlexRow, Footer, Header, Info, StudyTags } from './style';
 
 interface StudyInfoProps {
   studyId: string | string[] | undefined;
@@ -16,47 +17,55 @@ function StudyInfo({ studyId }: StudyInfoProps) {
     return null;
   }
 
-  const { name: title, description, tags: studyTags, users: members } = studyInfo;
+  const { name: title, description, tags: studyTags, users: members, startedAt, endedAt, progress } = studyInfo;
 
   return (
-    <Info>
-      <Header>
-        <Text
-          color="white"
-          variant="title01"
-          css={{ textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap' }}
-          as="h1"
-        >
-          {title}
-        </Text>
-      </Header>
-      <Body>
-        <Text color="gray020" variant="body02">
-          {description}
-        </Text>
-        <StudyTags>
-          <Tag.Row style={{ padding: 0 }}>
-            {studyTags && studyTags.map(({ id, name }) => <Tag key={id} value={name} />)}
-          </Tag.Row>
-        </StudyTags>
-      </Body>
-      <Footer>
-        {/* UserAvatar Group id가 여기서는 스터디 id가 되고 각 퀴즈에서는 퀴즈의 id가 됨 */}
-        <UserAvatar.Group view={6} id={studyId as string} groupType="study">
-          {members &&
-            members.map(({ userId, nickname, profileImageUrl }) => (
-              <UserAvatar
-                key={userId}
-                userName={nickname}
-                image={profileImageUrl}
-                size="large"
-                className={`avatar--user_${userId}`}
-                tooltip={true}
-              />
-            ))}
-        </UserAvatar.Group>
-      </Footer>
-    </Info>
+    <FlexRow>
+      <Info css={{ flex: 1 }}>
+        <Header>
+          <Text
+            color="white"
+            variant="title01"
+            css={{
+              textOverflow: 'ellipsis',
+              overflow: 'hidden',
+              whiteSpace: 'nowrap',
+              margin: '0 0 16px 0',
+            }}
+            as="h1"
+          >
+            {title}
+          </Text>
+        </Header>
+        <Body>
+          <Text color="gray020" variant="body02">
+            {description}
+          </Text>
+          <StudyTags>
+            <Tag.Row style={{ padding: 0 }}>
+              {studyTags && studyTags.map(({ id, name }) => <Tag key={id} value={name} />)}
+            </Tag.Row>
+          </StudyTags>
+        </Body>
+        <Footer>
+          {/* UserAvatar Group id가 여기서는 스터디 id가 되고 각 퀴즈에서는 퀴즈의 id가 됨 */}
+          <UserAvatar.Group view={6} id={studyId as string} groupType="study">
+            {members &&
+              members.map(({ userId, nickname, profileImageUrl }) => (
+                <UserAvatar
+                  key={userId}
+                  userName={nickname}
+                  image={profileImageUrl}
+                  size="large"
+                  className={`avatar--user_${userId}`}
+                  tooltip={true}
+                />
+              ))}
+          </UserAvatar.Group>
+        </Footer>
+      </Info>
+      <StudyProgress progress={progress} startedAt={startedAt} endedAt={endedAt} />
+    </FlexRow>
   );
 }
 
