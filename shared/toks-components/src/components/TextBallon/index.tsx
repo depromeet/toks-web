bc61aef4aa01227a54eb7cf61262ce3464cf14a3
goo <goo@tossbank.com>
2023-01-07 20:22:50 +0900

    feat: 툴팁 방향 bottom 구현

--- shared/toks-components/src/components/TextBallon/index.tsx
+++ shared/toks-components/src/components/TextBallon/index.tsx
@@ -4,10 +4,11 @@ import styled from '@emotion/styled';
 import { Text } from '../Text';
 
 // 다른 방향도 필요하면 추가.
-type Direction = 'top';
+type Direction = 'top' | 'bottom';
 
 interface Props {
   title: string;
+  width?: number;
   direction?: Direction;
   onClick?: () => void;
   className?: string;
@@ -17,9 +18,9 @@ interface Props {
 /**
  * @Note 아직 현재 필요한 기능만 구현한 상태입니다. 추가적인 디자인이 나오면, 추가 개발이 필요합니다.
  */
-export function TextBallon({ title, onClick, className, direction = 'top', color = 'primary' }: Props) {
+export function TextBallon({ title, onClick, className, direction = 'top', color = 'primary', width }: Props) {
   return (
-    <StyledBallon onClick={onClick} direction={direction} className={className} color={color}>
+    <StyledBallon onClick={onClick} direction={direction} className={className} color={color} style={{ width }}>
       <Text variant="body02" color="gray010" style={{ fontWeight: 700 }}>
         {title}
       </Text>
@@ -29,13 +30,28 @@ export function TextBallon({ title, onClick, className, direction = 'top', color
 
 export default TextBallon;
 
+const VALUE_BY_DIRECTION = {
+  top: {
+    translateY: '-150%',
+    arrowBorderTop: `10px solid ${theme.colors.primary}`,
+    arrowBorderBottom: `0px solid transparent`,
+    afterTop: '32px',
+  },
+  bottom: {
+    translateY: '0%',
+    arrowBorderTop: '0px solid transparent',
+    arrowBorderBottom: `10px solid ${theme.colors.primary}`,
+    afterTop: '-5px',
+  },
+};
+
 const StyledBallon = styled.div<{ direction: Direction; color: KeyOfColors }>`
   position: absolute;
   width: 80%;
   min-width: 220px;
   height: 36px;
   left: 50%;
-  transform: translate(-50%, -150%);
+  transform: ${({ direction }) => `translate(-50%, ${VALUE_BY_DIRECTION[direction].translateY})`};
   background: ${({ color }) => theme.colors[color]};
   color: ${theme.colors.gray010};
   border-radius: 32px;
@@ -43,15 +59,15 @@ const StyledBallon = styled.div<{ direction: Direction; color: KeyOfColors }>`
   text-align: center;
 
   &:after {
-    border-top: 10px solid ${theme.colors.primary};
+    border-top: ${({ direction }) => VALUE_BY_DIRECTION[direction].arrowBorderTop};
     border-left: 10px solid transparent;
     border-right: 10px solid transparent;
-    border-bottom: 0px solid transparent;
+    border-bottom: ${({ direction }) => VALUE_BY_DIRECTION[direction].arrowBorderBottom};
     content: '';
     position: absolute;
-    top: 32px;
+    top: ${({ direction }) => VALUE_BY_DIRECTION[direction].afterTop};
     left: 50%;
-    transform: translate(-50%, 0);
+    transform: translate(-50%, 0%);
   }
 
   &:hover {
