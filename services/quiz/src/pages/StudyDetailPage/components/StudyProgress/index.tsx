c89ffe846e388764f245a4c291dbeb179f39aaeb
dengoyoon <engus927@naver.com>
2023-01-07 15:35:19 +0900

    이미지 변경

--- services/quiz/src/pages/StudyDetailPage/components/StudyProgress/index.tsx
+++ services/quiz/src/pages/StudyDetailPage/components/StudyProgress/index.tsx
@@ -13,16 +13,12 @@ interface StudyProgressProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 const PROGRESS_IMG_BY_STEP = {
-  STEP0: 'https://blog.kakaocdn.net/dn/Yn8Ku/btrga1XCqZI/Qm9VZpwNw1k1HqqE6p5fL0/img.jpg',
-  STEP1:
-    'https://w.namu.la/s/5d951eed3bab16703e2ff271c370df13d7508b51a7fe4db7e3d2a87ae56e1328185bce27b70f3b7f8559f43cc8bedec51020c40307f8d696da921d9e75de53e01fd37289327397a1444a27db1d80d187',
-  STEP2: 'https://mblogthumb-phinf.pstatic.net/20130807_127/dlscjsqlsdl_1375853484362rxzgS_JPEG/yellow.jpg?type=w2',
-  STEP3:
-    'https://media.istockphoto.com/id/1160620218/photo/beautiful-nature-background-from-green-leaves-with-detailed-texture-greenery-top-view-closeup.jpg?s=612x612&w=0&k=20&c=hiwAxq9f9WD7dEw8SCckV6CJg0RHvqRo4mgbHA_g9Qo=',
-  STEP4:
-    'https://mblogthumb-phinf.pstatic.net/20160518_142/sooheeyoon_1463551725833ii4hv_JPEG/IMG_20160518_1.jpg?type=w800',
-  STEP5:
-    'https://mblogthumb-phinf.pstatic.net/MjAxNzA5MDRfMjYw/MDAxNTA0NTE0MjkyNjc3.0uFSATM11oD5o3GiaSYrinbl7TVHiZbK7iGwerqtygsg.EWVKNa4HynWuNzZoOKiaH4tUeb4YYClTmeC22vv3ejEg.JPEG.kdauuy/qhfktor4.jpg?type=w2',
+  STEP0: 'https://asset.tokstudy.com/progress-bar/img-progress-0.png',
+  STEP1: 'https://asset.tokstudy.com/progress-bar/img-progress-1.png',
+  STEP2: 'https://asset.tokstudy.com/progress-bar/img-progress-2.png',
+  STEP3: 'https://asset.tokstudy.com/progress-bar/img-progress-3.png',
+  STEP4: 'https://asset.tokstudy.com/progress-bar/img-progress-4.png',
+  STEP5: 'https://asset.tokstudy.com/progress-bar/img-progress-5.png',
 };
 
 export function StudyProgress({ progress, startedAt, endedAt }: StudyProgressProps) {
