9c95be7085bdab2aaa78593705d54e141f8074e6
chaaerim <cofla411@ewhain.net>
2023-01-08 22:17:40 +0900

    :sparkles: feat: quiz create page navbar 연결

--- service/src/quiz/pages/QuizCreatePage/index.tsx
+++ service/src/quiz/pages/QuizCreatePage/index.tsx
@@ -1,5 +1,7 @@
 import { FULL_HEIGHT } from '@depromeet/toks-components';
+import { useQueryParam } from '@depromeet/utils';
 import { Flex } from '@toss/emotion-utils';
+import { QuizNav } from 'quiz/common/components/QuizNav';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
@@ -11,35 +13,42 @@ import { QuizCreateForm } from './types';
 const QuizCreatePage = () => {
   const { register, setValue, control, getValues, setError } = useForm();
   const { createQuiz } = useQuizCreate();
+  const studyId = useQueryParam('studyId', { suspense: true });
+  if (!studyId) {
+    return null;
+  }
 
   return (
-    <Flex.Center css={{ height: FULL_HEIGHT }}>
-      <form
-        onSubmit={(e: React.FormEvent) => {
-          e.preventDefault();
-          const values = getValues() as QuizCreateForm;
+    <>
+      <QuizNav mainTitle={''} studyId={studyId} />
+      <Flex.Center css={{ height: FULL_HEIGHT }}>
+        <form
+          onSubmit={(e: React.FormEvent) => {
+            e.preventDefault();
+            const values = getValues() as QuizCreateForm;
 
-          if (!values.answer) {
-            setError('answer', {
-              type: 'required',
-              message: '답을 입력해주세요',
-            });
-            return;
-          }
+            if (!values.answer) {
+              setError('answer', {
+                type: 'required',
+                message: '답을 입력해주세요',
+              });
+              return;
+            }
 
-          createQuiz(values);
-        }}
-      >
-        <Flex
-          css={{
-            gap: '48px',
+            createQuiz(values);
           }}
         >
-          <QuizCreateEditor register={register} setValue={setValue} />
-          <QuizCreateInputList register={register} setValue={setValue} control={control} />
-        </Flex>
-      </form>
-    </Flex.Center>
+          <Flex
+            css={{
+              gap: '48px',
+            }}
+          >
+            <QuizCreateEditor register={register} setValue={setValue} />
+            <QuizCreateInputList register={register} setValue={setValue} control={control} />
+          </Flex>
+        </form>
+      </Flex.Center>
+    </>
   );
 };
 
