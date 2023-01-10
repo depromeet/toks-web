7bcb024cdb124562ab7060e94fa0dcefc2db7ad9
chaerim kim <89721027+chaaerim@users.noreply.github.com>
2023-01-11 04:03:18 +0900

    :sparkles: feat: 디자인 수정 및 답안 확인 시 answer 넘어오지 않는 현상 수정 (#200)

--- service/src/quiz/pages/QuizCheckingPage/index.tsx
+++ service/src/quiz/pages/QuizCheckingPage/index.tsx
@@ -6,7 +6,6 @@ import { useQuery } from 'react-query';
 import { QuizNav } from 'quiz/common/components/QuizNav';
 import { QuizQuestion } from 'quiz/common/components/QuizQuestion';
 import { getQuizById } from 'quiz/common/components/QuizQuestion/remotes/quiz';
-import { getQuizReplyById } from 'quiz/common/remotes/quizReply';
 import { getUser } from 'quiz/common/remotes/user';
 import { QUERY_KEYS } from 'quiz/constants/queryKeys';
 
@@ -15,31 +14,23 @@ import { AnswerCheckList } from './components/AnswerCheckList';
 export default function QuizCheckingPage() {
   const quizIdParams = usePathParam('quizIdParams', { suspense: true });
 
-  const { data: quizzes } = useQuery(
-    QUERY_KEYS.GET_QUIZREPLIES_BY_ID(quizIdParams),
-    () => getQuizReplyById(quizIdParams),
-    {
-      enabled: Boolean(quizIdParams),
-    }
-  );
   const { data: quiz } = useQuery(QUERY_KEYS.GET_QUIZ_BY_ID(quizIdParams), () => getQuizById(quizIdParams), {
     enabled: Boolean(quizIdParams),
   });
   const { data: user } = useQuery(QUERY_KEYS.GET_USER_INFO, getUser);
 
-  if (!quiz || !quizzes || !user) {
+  if (!quiz || !user) {
     return null;
   }
 
   const durationTime = calculateRemainingSecond(new Date(quiz.timestamp), new Date(quiz.endedAt));
 
-  const myAnswer = quizzes.quizReplyHistories.find(element => element.creator.nickname === user.nickname)?.answer;
   return (
     <>
       <QuizNav mainTitle="똑표 확인하기" studyId={quiz.studyId} />
       <Spacing size={25} />
       <Flex css={{ height: '100%' }}>
-        <QuizQuestion myAnswer={myAnswer} />
+        <QuizQuestion quizAnswer={quiz.answer} />
         <Flex css={{ width: '50%' }}>
           <AnswerCheckList durationTime={durationTime} />
         </Flex>
