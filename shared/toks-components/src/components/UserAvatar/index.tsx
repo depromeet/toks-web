138478e50e19bed6a5f1a1a3e8c1a405557b87f0
goo <goo@tossbank.com>
2023-01-13 22:10:04 +0900

    feat: 단일 아바타 툴팁 안뜨는 이슈

--- shared/toks-components/src/components/UserAvatar/index.tsx
+++ shared/toks-components/src/components/UserAvatar/index.tsx
@@ -42,6 +42,7 @@ export function UserAvatar({
   const isBaseProfileImage =
     image === 'http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_640x640.jpg';
   const baseProfileImage = 'https://toks-web-assets.s3.amazonaws.com/toks-emoji/ic-base-profile.png'; //"https://asset.tokstudy.com/toks-emoji/ic-base-profile.png";
+
   return (
     <>
       {tooltip && <Tooltip target={`.${className}`} content={tooltipContent} position="bottom" />}
