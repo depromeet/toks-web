5d05eae43937359db66b404e627dc691056515c9
chaerim kim <89721027+chaaerim@users.noreply.github.com>
2023-01-13 17:14:41 +0900

    :sparkles: feat: 퀴즈 출제자 플로우 추가 (#215)

--- service/src/quiz/pages/QuizVotingPage/index.tsx
+++ service/src/quiz/pages/QuizVotingPage/index.tsx
@@ -5,8 +5,11 @@ import { useQuery } from 'react-query';
 import { QuizNav } from 'quiz/common/components/QuizNav';
 import { QuizQuestion } from 'quiz/common/components/QuizQuestion';
 import { getQuizById } from 'quiz/common/components/QuizQuestion/remotes/quiz';
+import { getQuizReplyById } from 'quiz/common/remotes/quizReply';
+import { getUser } from 'quiz/common/remotes/user';
 import { QUERY_KEYS } from 'quiz/constants/queryKeys';
 
+import { EmptyAnswer } from '../QuizCheckingPage/components/EmptyAnswer';
 import { MyAnswerViewer } from './components/MyAnswerViewer';
 import { PeerAnswerList } from './components/PeerAnswerList';
 import { VoteSubmitButton } from './components/VoteSubmitButton';
@@ -17,21 +20,53 @@ export default function QuizVotingPage() {
   const { data: quiz } = useQuery(QUERY_KEYS.GET_QUIZ_BY_ID(quizIdParams), () => getQuizById(quizIdParams), {
     enabled: Boolean(quizIdParams),
   });
-  if (!quiz) {
+  const { data: user } = useQuery(QUERY_KEYS.GET_USER_INFO, getUser);
+
+  const { data: quizzes } = useQuery(
+    QUERY_KEYS.GET_QUIZREPLIES_BY_ID(quizIdParams),
+    () => getQuizReplyById(quizIdParams),
+    {
+      enabled: Boolean(quizIdParams),
+    }
+  );
+
+  if (!quiz || !user) {
     return null;
   }
-  return (
-    <>
-      <QuizNav mainTitle="똑표 하기" studyId={quiz.studyId} />
-      <Spacing size={25} />
-      <Flex css={{ height: '100%' }}>
-        <QuizQuestion />
-        <Flex direction="column" css={{ width: '50%', height: '100%', position: 'relative' }}>
-          <MyAnswerViewer />
-          <PeerAnswerList />
-          <VoteSubmitButton />
+  if (quiz.creator.nickname === user.nickname) {
+    return (
+      <>
+        <QuizNav mainTitle="똑표 하기" studyId={quiz.studyId} />
+        <Spacing size={25} />
+        <Flex css={{ height: '100%' }}>
+          <QuizQuestion quizAnswer={quiz.answer} />
+          <Flex direction="column" css={{ width: '50%', height: '80vh', position: 'relative' }}>
+            {quizzes?.quizReplyHistories.length !== 0 ? (
+              <>
+                <PeerAnswerList isQuizCreator={true} />
+                <VoteSubmitButton />
+              </>
+            ) : (
+              <EmptyAnswer />
+            )}
+          </Flex>
         </Flex>
-      </Flex>
-    </>
-  );
+      </>
+    );
+  } else {
+    return (
+      <>
+        <QuizNav mainTitle="똑표 하기" studyId={quiz.studyId} />
+        <Spacing size={25} />
+        <Flex css={{ height: '100%' }}>
+          <QuizQuestion />
+          <Flex direction="column" css={{ width: '50%', height: '80vh', position: 'relative' }}>
+            <MyAnswerViewer />
+            <PeerAnswerList isQuizCreator={false} />
+            <VoteSubmitButton />
+          </Flex>
+        </Flex>
+      </>
+    );
+  }
 }
