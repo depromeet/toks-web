131301d3bb9bfdde49f0b0518f47f9f33ff40ff9
chaaerim <cofla411@ewhain.net>
2023-01-17 17:53:38 +0900

    :bug: fix: yarn install

--- service/lib/gtag.ts
+++ service/lib/gtag.ts
@@ -5,7 +5,6 @@ declare global {
 }
 
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
-
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
   window.gtag('config', GA_TRACKING_ID, {
