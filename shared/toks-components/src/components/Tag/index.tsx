f319a9fcea13d4a91661288bf593957d3a8a4ad8
dengoyoon <engus927@naver.com>
2023-02-12 20:46:05 +0900

    ✨ feat: maxView 기능 추가

--- shared/toks-components/src/components/Tag/index.tsx
+++ shared/toks-components/src/components/Tag/index.tsx
@@ -1,15 +1,16 @@
 import { KeyOfColors, theme } from '@depromeet/theme';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import {css} from '@emotion/react';
-import { HTMLAttributes, ReactNode } from 'react';
+import { Children, HTMLAttributes, ReactElement, ReactNode } from 'react';
+
 import { Text } from '../Text';
 
-type TagColor = 'highlight' | 'normal'
+type TagColor = 'highlight' | 'normal';
 
 type TagColorMap = {
   [key in TagColor]: {
-    background: string,
-    color: KeyOfColors
+    background: string;
+    color: KeyOfColors;
   };
 };
 
@@ -18,18 +19,18 @@ interface TagProps extends HTMLAttributes<HTMLSpanElement> {
   color?: TagColor;
 }
 
-const TAG_COLOR : TagColorMap = {
-  normal : {
-    background : theme.colors.gray080,
-    color : 'gray020'
+const TAG_COLOR: TagColorMap = {
+  normal: {
+    background: theme.colors.gray080,
+    color: 'gray020',
   },
-  highlight : {
-    background : theme.colors.primary_opacity,
-    color : 'primary'
-  }
-}
+  highlight: {
+    background: theme.colors.primary_opacity,
+    color: 'primary',
+  },
+};
 
-const StyledTag = styled.span<{color : TagColor}>`
+const StyledTag = styled.span<{ color: TagColor }>`
   /* TODO : 이걸 살려서 적용이 되게 변경해야 함.
   padding: '4px 12px';
   width: 'fit-content';
@@ -39,11 +40,11 @@ const StyledTag = styled.span<{color : TagColor}>`
   letter-spacing: ' -0.6px';
   text-align: 'left';
   */
-  ${({color}) => css`
+  ${({ color }) => css`
     background: ${TAG_COLOR[color].background};
     color: ${TAG_COLOR[color].color};
   `}
-`
+`;
 
 export function Tag({ value, color = 'normal', ...restProps }: TagProps) {
   return (
@@ -58,18 +59,22 @@ export function Tag({ value, color = 'normal', ...restProps }: TagProps) {
       }}
       {...restProps}
     >
-      <Text variant='body02' color={TAG_COLOR[color].color}>{value}</Text>
+      <Text variant="body02" color={TAG_COLOR[color].color}>
+        {value}
+      </Text>
     </StyledTag>
   );
 }
 
 interface RowProps extends HTMLAttributes<HTMLUListElement> {
+  maxView?: number;
   children: ReactNode;
 }
 
 // TODO: maxView 개수 받도록
-function Row({ children, ...props }: RowProps) {
-  return <ListRow {...props}>{children}</ListRow>;
+function Row({ children, maxView, ...props }: RowProps) {
+  const tags = Children.toArray(children) as ReactElement[];
+  return <ListRow {...props}>{maxView ? tags.slice(0, maxView) : tags}</ListRow>;
 }
 
 Tag.Row = Row;
