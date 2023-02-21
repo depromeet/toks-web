dd77ec5b58ef24a5de10284e7a3491d401ebd326
minsgy <alstjrchl97@likelion.org>
2023-02-22 00:33:36 +0900

    ✨ feat: tooltip component 구현

---
+++ shared/toks-components/src/components/Tooltip/TooltipContent.tsx
@@ -0,0 +1,71 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { isBottom, PositionType, DirectionType } from '.';
+import { Text } from '../Text';
+
+export const TooltipContent = ({
+  message,
+  direction,
+  position,
+}: {
+  message: string;
+  direction: DirectionType;
+  position: PositionType;
+}) => {
+  return (
+    <StyledTooltipContent
+      direction={direction}
+      style={{
+        left: position.left,
+        top: position.top,
+      }}
+    >
+      <Text color="gray110" variant="body02">
+        {message}
+      </Text>
+    </StyledTooltipContent>
+  );
+};
+
+const StyledTooltipContent = styled('div')<{ direction: DirectionType }>`
+  &:before {
+    width: 0;
+    height: 0;
+    position: absolute;
+    border-style: solid;
+    ${({ direction }) => {
+      if (isBottom(direction)) {
+        return css`
+          border-width: 0 5px 10px 5px;
+          border-color: transparent transparent #efefef transparent;
+          left: 50%;
+          margin-left: -5px;
+          top: -10px;
+        `;
+      }
+      return css`
+        border-width: 5px 10px 5px 0px;
+        border-color: transparent #efefef transparent transparent;
+        top: 50%;
+        margin-top: -5px;
+        right: -10px;
+      `;
+    }};
+    content: '';
+  }
+
+  ${({ direction }) => {
+    if (isBottom(direction)) {
+      return css`
+        transform: translateX(-50%);
+      `;
+    }
+    return '';
+  }}
+
+  background-color: #efefef;
+  padding: 4px 16px;
+  position: absolute;
+  z-index: 9999;
+  border-radius: 9px;
+`;
---
+++ shared/toks-components/src/components/Tooltip/getTooltipPosition.ts
@@ -0,0 +1,22 @@
+import { RefObject } from 'react';
+
+// TODO: right 버전 추가
+export const getTooltipPosition = (ref: RefObject<HTMLDivElement>, gap = 5) => {
+  const { left: tooltipLeft, top: tooltipTop } = ref.current?.getBoundingClientRect() || {
+    left: 0,
+    top: 0,
+  };
+
+  const tooltipContentHeight = ref.current?.clientHeight ?? 0;
+  const tooltipContentWidth = ref.current?.clientWidth ?? 0;
+
+  const left = tooltipLeft + tooltipContentWidth / 2;
+
+  const tooltipArrow = tooltipTop + tooltipContentHeight + 10;
+  const top = tooltipArrow + gap;
+
+  return {
+    left,
+    top,
+  };
+};
