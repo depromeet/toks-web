e844df8563017499a2da013befee4640bc57c115
Minseok Choi <alstjrchl97@likelion.org>
2023-02-27 20:07:06 +0900

    feat(@deporomeet/toks-components): Tooltip 컴포넌트 prime-react 의존성 제거 및 리팩토링 (#256)
    * ✨ feat: portal dom 추가
    
    * ✨ feat: tooltip component 구현
    
    * ♻️ refactor: UserAvatar 툴팁 컴포넌트 재정의
    
    * 🐛 fix: tooltip lint fix

--- shared/toks-components/src/components/Tooltip/index.tsx
+++ shared/toks-components/src/components/Tooltip/index.tsx
@@ -1,72 +1,64 @@
-import { theme } from '@depromeet/theme';
-import styled from '@emotion/styled';
-import { Tooltip as BaseTooltip } from 'primereact/tooltip';
-import { ComponentProps } from 'react';
+import { useRef, useState } from 'react';
 
-type PositionType = 'top' | 'bottom' | 'right' | 'left';
+import { getTooltipPosition } from './getTooltipPosition';
+import { TooltipContent } from './TooltipContent';
+import { TooltipPortal } from './TooltipPortal';
 
-interface TooltipProps extends Omit<ComponentProps<typeof BaseTooltip>, 'target' | 'content' | 'position'> {
-  target: string;
-  content: string;
-  position: PositionType;
+export type DirectionType = 'bottom' | 'right';
+export interface PositionType {
+  left?: number;
+  top?: number;
 }
 
-const isBottom = (position: PositionType) => position === 'bottom';
+export const isBottom = (position: DirectionType) => position === 'bottom';
 
 /*
-TODO:
-- 툴팁 화살표 길이 늘어나게 구현해야함
-- 툴팁 방향에 따라 스타일 적용이 다른 부분에 대해서 중복성을 줄여야 함 
+  TODO: right 방향 구현 필요
 */
-export function Tooltip({ target, content, position }: TooltipProps) {
-  return isBottom(position) ? (
-    <StyledBottomTooltip
-      // TODO: inline style로 적용한 부분 제외하기
-      target={target}
-      content={content}
-      position={position}
-    />
-  ) : (
-    <StyledRightTooltip target={target} content={content} position={position} />
+
+export const Tooltip = ({
+  children,
+  message,
+  direction = 'bottom',
+}: {
+  children: React.ReactNode;
+  message: string;
+  direction?: DirectionType;
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [show, setShow] = useState(false);
+  const position = useRef<PositionType | null>(null);
+
+  const handleToggleTooltip = (isShow: boolean) => {
+    position.current = getTooltipPosition(ref);
+    setShow(isShow);
+  };
+
+  return (
+    <span
+      onFocus={() => handleToggleTooltip(true)}
+      onMouseLeave={() => handleToggleTooltip(false)}
+      onMouseOver={() => handleToggleTooltip(true)}
+      ref={ref}
+      style={{
+        display: 'inline-block',
+        minWidth: 0,
+        position: 'relative',
+      }}
+    >
+      {children}
+      {show && (
+        <TooltipPortal>
+          <TooltipContent
+            direction={direction}
+            message={message}
+            position={{
+              left: position.current?.left,
+              top: position.current?.top,
+            }}
+          />
+        </TooltipPortal>
+      )}
+    </span>
   );
-}
-
-const StyledBottomTooltip = styled(BaseTooltip)<{ position: PositionType }>`
-  text-align: center;
-  margin-top: 4px;
-  color: ${theme.colors.gray120} !important;
-
-  .p-tooltip {
-  }
-  .p-tooltip-text {
-    font-size: 14px;
-    color: ${theme.colors.gray120} !important;
-    background: ${theme.colors.white} !important;
-    padding: 4px 16px !important;
-    border-radius: 8px !important;
-  }
-
-  .p-tooltip-arrow {
-    border-bottom-color: ${theme.colors.white} !important;
-  }
-`;
-
-const StyledRightTooltip = styled(BaseTooltip)<{ position: PositionType }>`
-  text-align: center;
-  margin-left: 4px;
-  color: ${theme.colors.gray120} !important;
-
-  .p-tooltip {
-  }
-  .p-tooltip-text {
-    font-size: 14px;
-    color: ${theme.colors.gray120} !important;
-    background: ${theme.colors.white} !important;
-    padding: 4px 16px !important;
-    border-radius: 8px !important;
-  }
-
-  .p-tooltip-arrow {
-    border-right-color: ${theme.colors.white} !important;
-  }
-`;
+};
