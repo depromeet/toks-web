370645d1f9ebba5a01f7fcb6ca7be65d7f13ff3d
chaaerim <cofla411@ewhain.net>
2023-02-28 03:58:54 +0900

    :sparkles: feat: 드롭다운 레이아웃 작업

--- shared/toks-components/src/components/DropDown/index.tsx
+++ shared/toks-components/src/components/DropDown/index.tsx
@@ -1,40 +1,51 @@
 import { theme } from '@depromeet/theme';
-import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import { Dropdown, DropdownProps } from 'primereact/dropdown';
-import { forwardRef } from 'react';
+import { forwardRef, useState } from 'react';
+import { Icon } from '../Icon';
 
 import { Text } from '../Text';
 
-interface Props extends DropdownProps {
+type DropdownOption = {
+  label: string;
+  value: number;
+};
+
+interface DropdownProps {
   width?: number;
   height?: number;
   label?: string;
   errorMessage?: string;
+  value?: DropdownOption;
+  options: DropdownOption[];
 }
 
-export const DropDown = forwardRef<Dropdown, Props>(
-  ({ label, width, height, required, errorMessage, ...props }: Props, ref) => {
+export const DropDown = forwardRef<HTMLDivElement, DropdownProps>(
+  ({ value, options, ...props }: DropdownProps, ref) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectItem, setSelectItem] = useState('');
+    console.log(ref);
+
     return (
-      <DropDownWrapper>
-        {label && (
-          <Text variant="headline" className="label">
-            {label}
-            {required && '*'}
-          </Text>
-        )}
-        <StyledDropdown
-          ref={ref}
-          width={width}
-          height={height}
-          required={required}
-          isError={Boolean(errorMessage)}
-          {...props}
-        />
-        {errorMessage && (
-          <Text variant="body02" color="danger">
-            {errorMessage}
-          </Text>
+      <DropDownWrapper ref={ref} onBlur={() => setIsOpen(false)} onClick={() => setIsOpen(prev => !prev)}>
+        <span>
+          <Text variant="body02">{selectItem}</Text>
+        </span>
+        <div>{isOpen ? <Icon iconName="ic-chevron-up" /> : <Icon iconName="ic-chevron-down" />}</div>
+        {isOpen && (
+          <DropDownOptions>
+            {options.map(({ label, value }) => (
+              <DropDownOption
+                key={value}
+                onClick={e => {
+                  e.stopPropagation();
+                  setSelectItem(label);
+                  setIsOpen(false);
+                }}
+              >
+                <Text variant="body02">{label}</Text>
+              </DropDownOption>
+            ))}
+          </DropDownOptions>
         )}
       </DropDownWrapper>
     );
@@ -43,48 +54,36 @@ export const DropDown = forwardRef<Dropdown, Props>(
 
 const DropDownWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  gap: 8px;
+  position: relative;
+  width: 100%;
+  min-height: 1.5rem;
+  background: ${theme.colors.gray100};
+  align-items: center;
+  padding: 11px 16px;
+  border-radius: 8px;
+  outline: none;
+  justify-content: space-between;
 `;
 
-const StyledDropdown = styled(Dropdown)<{ isError?: boolean }>`
-  border-radius: 8px !important;
-  padding: 14px 16px;
-  background: ${theme.colors.gray100} !important;
-  outline: none !important;
-  list-style: none !important;
-  box-shadow: none !important;
+const DropDownOptions = styled.ul`
+  margin: 0;
+  padding: 0;
+  background: ${theme.colors.gray100};
+  width: 100%;
+  position: absolute;
+  list-style: none;
+  max-height: 200px;
+  overflow-y: auto;
+  border-radius: 8px;
+  top: calc(100% + 4px);
+  left: 0;
+  z-index: 1010;
+`;
 
-  ${props => {
-    const { width, height, isError } = props;
-
-    return css`
-      border: ${isError ? `1px solid ${theme.colors.danger} !important` : 'none !important'};
-      width: ${width ? `${width}px` : '100%'};
-      height: ${height ? `${height}px` : '48px'};
-    `;
-  }}
-
-  .p-placeholder {
-    color: ${theme.colors.gray070};
-    padding: 0;
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 20px;
-    font-family: 'Spoqa Han Sans Neo' !important ;
+const DropDownOption = styled.li`
+  :hover {
+    background: ${theme.colors.gray070};
   }
-
-  .p-inputtext {
-    padding: 0;
-    color: ${theme.colors.white};
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 20px;
-  }
-
-  .p-dropdown-trigger-icon {
-    padding-right: 6px;
-    color: transparent !important;
-    background-image: url('https://toks-web-assets.s3.amazonaws.com/disabledTrigger.svg');
-  }
+  padding: 13px 16px;
+  cursor: pointer;
 `;
