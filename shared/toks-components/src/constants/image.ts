2b275eca308f485da871a6d25bda04263c26845b
dengoyoon <engus927@naver.com>
2023-03-08 12:00:51 +0900

    ♻️ refactor: 똑스 기본 프로필 사진, 카카오 기본 프로필 공통 상수로 빼고 이 값으로 대체

---
+++ shared/toks-components/src/constants/image.ts
@@ -0,0 +1,4 @@
+export const image = {
+  baseToks: 'https://asset.tokstudy.com/toks-emoji/ic-base-profile.png',
+  baseKakao: 'http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_640x640.jpg',
+};
--- shared/toks-components/src/constants/index.ts
+++ shared/toks-components/src/constants/index.ts
@@ -1,2 +1,3 @@
 export * from './emoji';
 export * from './env';
+export * from './image';
