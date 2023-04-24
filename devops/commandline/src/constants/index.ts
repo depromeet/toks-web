37c3381cdac91f1585542d5e37ca38d9eed6ed23
dengoyoon <engus927@naver.com>
2023-04-24 16:25:07 +0900

    🔥 remove: commandline을 사용하기 위해 수정한 부분 삭제

--- devops/commandline/src/constants/index.ts
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-
-export const ROOT_DIR = path.resolve(__dirname, '../../../');
-
-export const TEST_SCRIPTS = {
-  static_analysis: ['typecheck', 'lint', 'find-deadcode'],
-  test: ['test'],
-};
