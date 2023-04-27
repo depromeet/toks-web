51b96cebc87fb0f6d71ded3ee515fbc2805800ee
DuHyeon Yoon <engus927@naver.com>
2023-04-27 15:50:45 +0900

    feat(cd) : 개발서버 배포, 자동 배포 액션 구축 (#280)
    * ✨ feat: 배포스크립트 작성
    
    * 📦 chore: 의존성 설치
    
    * ✅ test: feat/cd의 코드로 개발서버 배포 성공
    
    * 📝 docs: 개벌서버 CD 작성
    
    * 🐛 fix: 시크릿 키 이름 수정
    
    * ✅ test: 되라 되라 되라
    
    * ✅ test: test
    
    * ✅ test: test
    
    * ✅ test: test
    
    * 📝 docs: target수정
    
    * ✅ test: test
    
    * 📝 docs: CD를 dev브랜치에서 일어나게 변경
    
    * ✅ test: test

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Dhy.js</a>
         </h1>
         <ExampleComponentCSS>TEST</ExampleComponentCSS>
         <p className={styles.description}>
