0b5ec4d0b8b957b8cd2c78a32b7be0557a09d679
chaaerim <cofla411@ewhain.net>
2023-05-08 02:54:05 +0900

    :bug: fix: storybook main.js 수정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,6 @@ module.exports = {
   },
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/addon-a11y',
     '@storybook/addon-essentials',
     '@storybook/addon-links',
     {
@@ -16,4 +15,11 @@ module.exports = {
       },
     },
   ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
 };
