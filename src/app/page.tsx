51f2ec149a4b098e26463cf83c8a4dfedee08ec6
chaaerim <cofla411@ewhain.net>
2023-06-20 23:25:50 +0900

    :recycle: refactor: 상위 style 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { color } from '@/components/foundation/color';
 
 export default function Home() {
   return (
-    <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-      <h1 className={color({ bgColor: 'gray20', textColor: 'success' })}>
+    <div>
+      <h1 className={color({ bgColor: 'primaryPress', textColor: 'success' })}>
         Toks
       </h1>
     </div>
