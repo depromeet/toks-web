47eea97ca72f7c119601623bebaa241cc960a128
Minseok Choi <alstjrchl97@likelion.org>
2023-06-30 16:33:23 +0900

    refactor: tailwind variant remove + clsx 세팅 (#292)
    * refactor: tailwind variant remove + clsx 세팅
    
    * fix: lint fix error 해결 및 의존성 추가 다운로드
    
    * :package: chore: netlify build ci test
    
    * :package: chore: storybook ci test
    
    ---------
    
    Co-authored-by: morri.k <kakao_ent@kakao-entui-MacBookPro.local>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,7 @@
-import { tv } from 'tailwind-variants';
-
-import { color } from '@/common/foundation/color';
-import { shadow } from '@/common/foundation/shadow';
-
-const button = tv({
-  variants: {
-    color,
-    shadow,
-  },
-});
 export default function Home() {
   return (
     <div>
-      <button className={button({ shadow: 'book01' })}>Toks</button>
+      <button className="flex bg-slate-300">Toks</button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
-const { withTV } = require('tailwind-variants/transformer');
-
 const { colors } = require('./style/theme/colors');
 const { shadows } = require('./style/theme/shadows');
-
 /** @type {import('tailwindcss').Config} */
-module.exports = withTV({
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: { colors, shadows },
+    extend: {colors , boxShadow: shadows},
   },
   plugins: [],
-});
+}
+
