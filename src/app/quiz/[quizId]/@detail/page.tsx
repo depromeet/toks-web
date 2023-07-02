3ed381824ee5e1014d8cc6efaed93ed84c976768
dengoyoon <engus927@naver.com>
2023-07-02 19:56:55 +0900

    ✨ feat: 테스트 삼아서 퀴즈 디테일 영역에 목 데이터 불러옴

--- src/app/quiz/[quizId]/@detail/page.tsx
+++ src/app/quiz/[quizId]/@detail/page.tsx
@@ -4,8 +4,36 @@ type Props = {
   };
 };
 
-function DetailPage({ params: { quizId } }: Props) {
-  return <div>Detail Page : {quizId}</div>;
+// 임시 타입
+type QuizType = {
+  userId: string;
+  id: string;
+  title: string;
+  completed: boolean;
+};
+
+const getQuizDetail = async (quizId: string) => {
+  const result = await fetch(
+    // 테스트로 가져온 투두데이터
+    `https://jsonplaceholder.typicode.com/todos/${quizId}`
+  );
+  const todo: QuizType = await result.json();
+  return todo;
+};
+
+async function DetailPage({ params: { quizId } }: Props) {
+  const quizDetailData = await getQuizDetail(quizId);
+  return (
+    <div style={{ backgroundColor: '#d18760' }}>
+      <h1>퀴즈 디테일 영역 입니다.</h1>
+      <div>아이디 가져오기 가능 Quiz Id : {quizId}</div>
+      <div>Todo Title : {quizDetailData.title}</div>
+      <div>
+        Completed :
+        {quizDetailData.completed ? <span> Yes</span> : <span> No</span>}
+      </div>
+    </div>
+  );
 }
 
 export default DetailPage;
