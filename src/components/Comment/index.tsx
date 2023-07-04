7181e032bb9f1fb9421ca1f880299d2de4505815
dengoyoon <engus927@naver.com>
2023-07-05 01:41:09 +0900

    ✨ feat: Comment 컴포넌트 스타일링 제외 뼈대 생성

---
+++ src/components/Comment/index.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+
+import { Text } from '../Text';
+
+interface CommentProps {
+  profileImgUrl?: string;
+  name: string;
+  timeAgo: string;
+  comment: string;
+  like: number;
+}
+
+export function Comment({
+  profileImgUrl,
+  name,
+  timeAgo,
+  comment,
+  like,
+}: CommentProps) {
+  const baseIcon =
+    'https://toks-web-assets.s3.amazonaws.com/emoji/ic_base-grey.svg';
+  return (
+    <div>
+      <div>
+        <Image src={profileImgUrl ?? baseIcon} alt="프로필 아이콘" />
+        <Text typo="bodyBold" color="white">
+          {name}
+        </Text>
+        <Text typo="caption" color="gray60">
+          {timeAgo}
+        </Text>
+      </div>
+      <Text typo="body" color="gray20">
+        {comment}
+      </Text>
+      <Text typo="body" color="white">
+        {like}
+      </Text>
+    </div>
+  );
+}
