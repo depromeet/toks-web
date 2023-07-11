4eb12f73507fcb932b7a6977d6fdedb56c502f85
dengoyoon <engus927@naver.com>
2023-07-11 19:18:54 +0900

    🎨 style: 페이지 요소 간격 퍼블리싱

--- src/app/quiz/[quizId]/@comment/page.tsx
+++ src/app/quiz/[quizId]/@comment/page.tsx
@@ -11,22 +11,25 @@ type Props = {
 
 async function CommentPage({ params: { quizId } }: Props) {
   const comments = await getCommentsByQuizId(quizId);
+  const isCommentEmpty = comments.length === 0;
   return (
-    <div className={clsx('flex', 'flex-col', 'mt-8')}>
-      <Comment.List>
-        {comments.map(({ id, nickname, content, likeCount, createdAt }) => (
-          <Comment
-            key={id}
-            commentId={id}
-            name={nickname}
-            comment={content}
-            timeAgo={createdAt}
-            profileImgUrl={undefined}
-            like={likeCount}
-          />
-        ))}
-      </Comment.List>
-      <GetStartedButton isCommentEmpty={comments.length === 0} />
+    <div className={clsx('flex', 'flex-col', 'gap-8', 'mt-8')}>
+      {!isCommentEmpty && (
+        <Comment.List>
+          {comments.map(({ id, nickname, content, likeCount, createdAt }) => (
+            <Comment
+              key={id}
+              commentId={id}
+              name={nickname}
+              comment={content}
+              timeAgo={createdAt}
+              profileImgUrl={undefined}
+              like={likeCount}
+            />
+          ))}
+        </Comment.List>
+      )}
+      <GetStartedButton isCommentEmpty={isCommentEmpty} />
     </div>
   );
 }
