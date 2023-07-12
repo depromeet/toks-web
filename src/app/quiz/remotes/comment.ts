3ce828bb41c217dc825414b1cf11effd31ab0605
dengoyoon <engus927@naver.com>
2023-07-12 18:24:38 +0900

    ✨ feat: 퀴즈 생성 함수 구현

--- src/app/quiz/remotes/comment.ts
+++ src/app/quiz/remotes/comment.ts
@@ -1,4 +1,4 @@
-import { CommentType } from '../models/comment';
+import { CommentType } from '@/app/quiz/models/comment';
 
 export const getCommentsByQuizId = async (quizId: string) => {
   const comments: CommentType[] = await fetch(
@@ -8,4 +8,25 @@ export const getCommentsByQuizId = async (quizId: string) => {
     .then((result) => result.json())
     .then((commentInfo) => commentInfo.data.content);
   return comments;
+};
+
+export const postCommentByQuizId = async (quizId: string, comment: string) => {
+  const DUHYEON_TOKEN =
+    'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJlbmd1czA5MjdAbmF0ZS5jb20iLCJ1aWQiOjIsImlhdCI6MTY4ODIyNTcxNiwiaXNzIjoiMiIsImV4cCI6MTcxOTc2MTcxNn0.skBi7GdIbGeUAPv8D2ZKhTOov4kb0woQAtwnLFK2Hec';
+  const data: CommentType = await fetch(
+    `https://api.tokstudy.com/api/v1/quizzes/${quizId}/comments`,
+    {
+      cache: 'no-store',
+      method: 'POST',
+      headers: {
+        'X-TOKS-AUTH-TOKEN': DUHYEON_TOKEN,
+      },
+      body: JSON.stringify({
+        comment,
+      }),
+    }
+  )
+    .then((result) => result.json())
+    .then((commentInfo) => commentInfo.data);
+  return data;
 };
