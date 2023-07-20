b9bd77b024235521f8bc609505877e9c7ff24a86
dengoyoon <engus927@naver.com>
2023-07-21 02:34:53 +0900

    ✨ feat: Swiper 정상 빌드 확인 성공

---
+++ src/common/components/Carousel/index.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import SwiperCore from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import { QuizCard } from '@/common';
+
+import 'swiper/css';
+
+export function Carousel() {
+  const settings: SwiperCore = {
+    spaceBetween: 8,
+    slidesPerView: 1.07,
+  };
+  return (
+    <Swiper {...settings}>
+      <SwiperSlide>
+        <QuizCard
+          categoryTitle="카테고리"
+          quizDescription="Title Text Title Text Title Text Title Text"
+          images={['https://source.unsplash.com/random/?programming']}
+          sizeType="small"
+          likeCount={10}
+          commentCount={10}
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <QuizCard
+          categoryTitle="카테고리"
+          quizDescription="Title Text Title Text Title Text Title Text"
+          images={['https://source.unsplash.com/random/?programming']}
+          sizeType="small"
+          likeCount={10}
+          commentCount={10}
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <QuizCard
+          categoryTitle="카테고리"
+          quizDescription="Title Text Title Text Title Text Title Text"
+          images={[
+            'https://source.unsplash.com/random/?programming',
+            'https://source.unsplash.com/random/daily',
+          ]}
+          likeCount={10}
+          commentCount={10}
+          sizeType="small"
+          quizType="ox"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <QuizCard
+          categoryTitle="카테고리"
+          quizDescription="Title Text Title Text Title Text Title Text"
+          likeCount={10}
+          commentCount={10}
+          images={['https://source.unsplash.com/random/?programming']}
+          sizeType="small"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <QuizCard
+          categoryTitle="카테고리"
+          quizDescription="Title Text Title Text Title Text Title Text"
+          likeCount={10}
+          commentCount={10}
+          quizType="ox"
+          sizeType="small"
+        />
+      </SwiperSlide>
+    </Swiper>
+  );
+}
