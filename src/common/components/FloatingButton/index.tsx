497f515faf22ebedc91594002707f2932fbead09
chaaerim <cofla411@ewhain.net>
2023-07-31 23:21:57 +0900

    :art: style: add hover pointer

--- src/common/components/FloatingButton/index.tsx
+++ src/common/components/FloatingButton/index.tsx
@@ -4,7 +4,7 @@ import { ICON_URL } from '@/common/constants';
 
 export function FloatingButton() {
   return (
-    <div className="flex h-50px w-50px flex-1 items-center justify-center rounded-full border-1px border-gray-70 bg-gray-90">
+    <div className="flex h-50px w-50px flex-1 items-center justify-center rounded-full border-1px border-gray-70 bg-gray-90 hover:cursor-pointer">
       <Image
         src={ICON_URL.EMOJI_DROOLING}
         alt="똑스 아이콘"
