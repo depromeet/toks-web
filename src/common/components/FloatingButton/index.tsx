957db364f1cc086e86fc72952c3392c4fe45d6ae
chaaerim <cofla411@ewhain.net>
2023-08-03 22:24:51 +0900

    fix: 시맨틱 태그 적용

--- src/common/components/FloatingButton/index.tsx
+++ src/common/components/FloatingButton/index.tsx
@@ -4,13 +4,13 @@ import { ICON_URL } from '@/common/constants';
 
 export function FloatingButton() {
   return (
-    <div className="z-99 flex h-50px w-50px flex-1 items-center justify-center rounded-full border-1px border-gray-70 bg-gray-90 hover:cursor-pointer">
+    <button className="z-99 flex h-50px w-50px flex-1 items-center justify-center rounded-full border-1px border-gray-70 bg-gray-90 hover:cursor-pointer">
       <Image
         src={ICON_URL.EMOJI_DROOLING}
         alt="똑스 아이콘"
         width={32}
         height={32}
       />
-    </div>
+    </button>
   );
 }
