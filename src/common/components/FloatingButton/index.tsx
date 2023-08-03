6f2123b66ff8926e3356e9d6fe9b833030f3ee9c
chaaerim <cofla411@ewhain.net>
2023-08-04 00:05:39 +0900

    :sparkles: feat: change to button tag

--- src/common/components/FloatingButton/index.tsx
+++ src/common/components/FloatingButton/index.tsx
@@ -1,10 +1,20 @@
+import clsx from 'clsx';
 import Image from 'next/image';
 
 import { ICON_URL } from '@/common/constants';
 
-export function FloatingButton() {
+export function FloatingButton({
+  className,
+  ...rest
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
-    <button className="z-99 flex h-50px w-50px flex-1 items-center justify-center rounded-full border-1px border-gray-70 bg-gray-90 hover:cursor-pointer">
+    <button
+      className={clsx(
+        'z-99 flex h-50px w-50px flex-1 items-center justify-center rounded-full border-1px border-gray-70 bg-gray-90 hover:cursor-pointer',
+        className
+      )}
+      {...rest}
+    >
       <Image
         src={ICON_URL.EMOJI_DROOLING}
         alt="똑스 아이콘"
