8789fa886b8f0b628ab56fff5a6d1380feb7d49d
Minseok Choi <alstjrchl97@likelion.org>
2023-08-05 14:53:27 +0900

    refactor: Text 컴포넌트 type 받도록 수정 (#318)
    Co-authored-by: socar-riaco <riaco@socar.kr>

--- src/common/components/Text/index.tsx
+++ src/common/components/Text/index.tsx
@@ -1,27 +1,36 @@
-import clsx from 'clsx';
-import { HTMLAttributes, ReactNode } from 'react';
+import { HTMLAttributes, ReactNode, createElement } from 'react';
 
-import { KeyOfColor, KeyOfTypography, textColor, typography } from '@/common';
+import {
+  KeyOfColor,
+  KeyOfTypography,
+  cn,
+  textColor,
+  typography,
+} from '@/common';
+
+import { TEXT_TAGS } from './constants';
 
 interface TextProps extends HTMLAttributes<HTMLSpanElement> {
   color?: KeyOfColor;
   typo: KeyOfTypography;
   children: ReactNode;
+  as?: keyof typeof TEXT_TAGS;
 }
 
 export function Text({
   color = 'white',
   children,
   typo,
+  as = 'span',
   className,
   ...rest
 }: TextProps) {
-  return (
-    <span
-      className={clsx(typography[typo], textColor[color], className)}
-      {...rest}
-    >
-      {children}
-    </span>
+  return createElement(
+    as,
+    {
+      className: cn(`${textColor[color]} ${typography[typo]}`, className),
+      ...rest,
+    },
+    [children]
   );
 }
