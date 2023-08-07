011e9b70cd68acf6379069424b2ac063a9661aa3
dengoyoon <engus927@naver.com>
2023-08-08 04:05:05 +0900

    ✨ feat: 버튼 기능 구현

--- src/app/quiz/components/ScrollToTopButton/index.tsx
+++ src/app/quiz/components/ScrollToTopButton/index.tsx
@@ -2,16 +2,39 @@
 
 import clsx from 'clsx';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 import { ICON_URL, bgColor } from '@/common';
 
 export function ScrollToTopButton() {
+  const [isVisible, setIsVisible] = useState(false);
+  const handleScroll = () => {
+    if (window.scrollY > 1000 && !isVisible) {
+      setIsVisible(true);
+    }
+    if (window.scrollY < 1000 && isVisible) {
+      setIsVisible(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  });
   return (
     <button
       className={clsx(
         bgColor['gray20'],
         'fixed bottom-36px right-20px z-10 flex h-40px w-40px items-center justify-center rounded-40px'
       )}
+      onClick={() => {
+        window.scrollTo(0, 0);
+      }}
+      style={{
+        opacity: isVisible ? 1 : 0,
+        transition: 'opacity 0.2s ease-in-out',
+      }}
     >
       <Image
         src={ICON_URL.CHEVRON_UP}
