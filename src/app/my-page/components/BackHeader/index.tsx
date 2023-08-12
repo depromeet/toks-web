6fd4a710a5ed4e8d3e672b5206d8e705e18e52e3
chaaerim <cofla411@ewhain.net>
2023-08-12 16:52:37 +0900

    :sparkles: feat: Appbar 분기처리

---
+++ src/app/my-page/components/BackHeader/index.tsx
@@ -0,0 +1,7 @@
+type BackHeaderProps = {
+  urlPath: string;
+};
+
+export const BackHeader = ({ urlPath }: BackHeaderProps) => {
+  return <h1>hi</h1>;
+};
---
+++ src/app/my-page/page.tsx
@@ -0,0 +1,5 @@
+const MyPage = () => {
+  return <h1>hi</h1>;
+};
+
+export default MyPage;
