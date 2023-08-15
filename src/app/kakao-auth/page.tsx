e831dfe997777adbd68e6b81199f02121237bc31
chaerim kim <89721027+chaaerim@users.noreply.github.com>
2023-08-15 22:11:08 +0900

    feat(nickname): 닉네임 설정 로직 구현 (#322)
    * fix: tooltip 꼬리 안나오는 현상 수정
    
    * :bug: fix: 헤더 오타 수정
    
    * :sparkles: feat: 닉네임 설정 여부에 따라 리다이렉트
    
    * :sparkles: feat: custom axios error to toks error
    
    * :sparkles: feat: add react hook form
    
    * :sparkles: feat: useCreateNicknameForm 구현
    
    * :sparkles: feat: post nickname api fetching
    
    * :sparkles: feat: 중복 닉네임 에러 처리
    
    * :recycle: refactor: delete export
    
    * :bug: fix: 닉네임 에러 메세지 수정
    
    * :recycle: refactor: add export http
    
    * :bug: fix: 마이페이지 코드 삭제

--- src/app/kakao-auth/page.tsx
+++ src/app/kakao-auth/page.tsx
@@ -1,15 +1,20 @@
 'use client';
 
+import { useQuery } from '@tanstack/react-query';
 import { deleteCookie, setCookie } from 'cookies-next';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+import { isToksError } from '@/common/utils/http';
+import { getUserInfo } from '@/queries';
 
 const KakaoAuth = () => {
   const params = useSearchParams();
   const router = useRouter();
+  const [accessToken, setAccessToken] = useState<string | null>('');
 
   useEffect(() => {
-    const accessToken = params.get('accessToken');
+    setAccessToken(params.get('accessToken'));
     const refreshToken = params.get('refreshToken');
 
     if (accessToken && refreshToken) {
@@ -17,9 +22,25 @@ const KakaoAuth = () => {
       deleteCookie('refreshToken');
       setCookie('accessToken', accessToken);
       setCookie('refreshToken', refreshToken);
-      router.push('/nickname');
     }
-  }, [params, router]);
+  }, [params, router, accessToken]);
+
+  const { data: user, isError } = useQuery(
+    ['userInfo', accessToken],
+    async () => {
+      try {
+        return await getUserInfo();
+      } catch (err: unknown) {
+        if (isToksError(err) && err.status === 404) {
+          router.replace('/nickname');
+        }
+      }
+    }
+  );
+
+  if (user && !isError) {
+    router.replace('/toks-main');
+  }
 
   return null;
 };
