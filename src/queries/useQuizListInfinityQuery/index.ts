c08def2af6a3a6cba27eeb6b9b2b4797bfc55771
dengoyoon <engus927@naver.com>
2023-08-18 19:57:27 +0900

    🐛 fix: 백엔드 데이터 변경 (1,2 -> A,B,O,X)에 의한 빌드 에러 해결

--- src/queries/useQuizListInfinityQuery/index.ts
+++ src/queries/useQuizListInfinityQuery/index.ts
@@ -26,12 +26,17 @@ export const useQuizListInfinityQuery = () => {
         if (quiz.question.imageUrl) {
           imageArray.push(quiz.question.imageUrl);
         }
-        if (quiz.question.buttons[1].imageUrl) {
-          imageArray.push(quiz.question.buttons[1].imageUrl);
-        }
-        if (quiz.question.buttons[2].imageUrl) {
-          imageArray.push(quiz.question.buttons[2].imageUrl);
-        }
+        // if (quiz.question.buttons[1].imageUrl) {
+        //   imageArray.push(quiz.question.buttons[1].imageUrl);
+        // }
+        // if (quiz.question.buttons[2].imageUrl) {
+        //   imageArray.push(quiz.question.buttons[2].imageUrl);
+        // }
+        imageArray.push(
+          ...(Object.values(quiz.question.buttons)
+            .map((button) => button?.imageUrl)
+            .filter((imageUrl) => imageUrl !== undefined) as string[])
+        );
         return imageArray;
       };
 
