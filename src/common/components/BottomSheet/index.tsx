3ca870cf1b8968658b4e31b97811db07b6e6ea0d
dengoyoon <engus927@naver.com>
2023-08-26 19:30:12 +0900

    🐛 fix: BottomSheet사용에 생긴 문제들 해결

--- src/common/components/BottomSheet/index.tsx
+++ src/common/components/BottomSheet/index.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import clsx from 'clsx';
 import React, { PropsWithChildren } from 'react';
 
-import { useClickAway } from '@/common/hooks/useClickAway';
+// import { useClickAway } from '@/common/hooks/useClickAway';
+import { cn } from '@/common/utils';
 
 import { BottomSheetProps } from './types';
 import { GlobalPortal } from '../GlobalPortal';
@@ -13,24 +13,51 @@ export const BottomSheet = ({
   children,
   onClose,
 }: PropsWithChildren<BottomSheetProps>) => {
-  const bottomSheetContentRef = useClickAway({
-    callback: () => {
-      onClose();
-    },
-  });
+  // const bottomSheetContentRef = useClickAway({
+  //   callback: () => {
+  //     onClose();
+  //   },
+  // });
   return (
     <GlobalPortal.Consumer>
+      <Dimmer isShow={isShow} onClose={onClose} />
       <div
-        ref={bottomSheetContentRef}
-        className={clsx(
-          'fixed bottom-0 left-0 right-0 z-50 h-486px w-full translate-y-full rounded-tl-16px rounded-tr-16px bg-gray-90 transition-transform duration-75',
+        // ref={bottomSheetContentRef}
+        style={{
+          transition: 'transform 0.3s ease-out',
+        }}
+        className={cn(
+          'fixed bottom-0 left-0 right-0 z-50 h-486px w-full translate-y-full rounded-tl-16px rounded-tr-16px bg-gray-90',
           {
-            'translate-y-0 animate-slide-up-bottom-sheet': isShow,
+            'translate-y-0': isShow, //animate-slide-up-bottom-sheet
           }
         )}
       >
         {children}
       </div>
     </GlobalPortal.Consumer>
+  );
+};
+
+const Dimmer = ({
+  isShow,
+  onClose,
+}: {
+  isShow: boolean;
+  onClose: VoidFunction;
+}) => {
+  return (
+    <div
+      onClick={() => {
+        onClose();
+      }}
+      className={cn(
+        'fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-gray-120/80',
+        {
+          hidden: !isShow,
+          'animate-fade-in-back-drop': isShow,
+        }
+      )}
+    />
   );
 };
