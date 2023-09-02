9bfa8cbf3e05991527519424507232daffe6f50b
chaaerim <cofla411@ewhain.net>
2023-09-03 00:58:10 +0900

    :sparkles: feat: logout bottomsheet 구현 완료

--- src/app/my-page/components/LogoutBar/index.tsx
+++ src/app/my-page/components/LogoutBar/index.tsx
@@ -9,24 +9,28 @@ import { LogoutBottomSheet } from '../LogoutBottonSheet';
 
 export const LogoutBar = () => {
   const [isShow, setIsShow] = useState(false);
-  console.log();
+  console.log(isShow);
 
   return (
-    <button
-      onClick={() => setIsShow(true)}
-      // onClick={() => onClick()}
-      className="flex h-full w-full items-center justify-between rounded-12px bg-gray-100 px-20px py-16px"
-    >
-      <Text typo="subheadingBold" color="gray10">
-        로그아웃
-      </Text>
-      <Image
-        src={ICON_URL.CHEVRON_RIGHT}
-        alt="로그아웃 버튼"
-        width={24}
-        height={24}
-      />
-      <LogoutBottomSheet onClick={() => setIsShow(false)} isShow={isShow} />
-    </button>
+    <div>
+      <button
+        onClick={() => {
+          setIsShow(true);
+        }}
+        // onClick={() => onClick()}
+        className="flex h-full w-full items-center justify-between rounded-12px bg-gray-100 px-20px py-16px"
+      >
+        <Text typo="subheadingBold" color="gray10">
+          로그아웃
+        </Text>
+        <Image
+          src={ICON_URL.CHEVRON_RIGHT}
+          alt="로그아웃 버튼"
+          width={24}
+          height={24}
+        />
+      </button>
+      <LogoutBottomSheet onClose={() => setIsShow(false)} isShow={isShow} />
+    </div>
   );
 };
--- src/app/my-page/components/LogoutBottonSheet/index.tsx
+++ src/app/my-page/components/LogoutBottonSheet/index.tsx
@@ -1,18 +1,19 @@
 import { useMutation } from '@tanstack/react-query';
 import { deleteCookie } from 'cookies-next';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
-import { BottomSheet, Button, Text } from '@/common';
+import { BottomSheet, Button, ICON_URL, Text } from '@/common';
 
 import { patchLogout } from '../../remotes/logout';
 
 interface LogoutBottomSheetProps {
-  onClick: () => void;
+  onClose: VoidFunction;
   isShow: boolean;
 }
 
 export const LogoutBottomSheet = ({
-  onClick,
+  onClose,
   isShow,
 }: LogoutBottomSheetProps) => {
   const router = useRouter();
@@ -23,23 +24,30 @@ export const LogoutBottomSheet = ({
       deleteCookie('refreshToken');
       //   TODO: path name 바뀔수도
       router.push('/toks-main');
-
       return res;
     } catch (err: unknown) {
       console.log(err);
     }
   });
   const onLogout = async () => {
-    onClick();
+    onClose();
     logoutMutation();
-    console.log('logout');
   };
   return (
-    <BottomSheet onClose={() => onClick()} isShow={isShow}>
+    <BottomSheet onClose={() => onClose()} isShow={isShow}>
       <div className="px-20px py-26px">
-        <Text typo="headingL" color="gray10">
-          로그아웃 하시겠어요?
-        </Text>
+        <div className="flex justify-between">
+          <Text typo="headingL" color="gray10">
+            로그아웃 하시겠어요?
+          </Text>
+          <Image
+            onClick={() => onClose()}
+            src={ICON_URL.SMALL_X}
+            alt="바텀시트 닫기버튼"
+            width={14}
+            height={14}
+          />
+        </div>
         <div className="h-40px" />
         <Button
           className="w-full"
@@ -52,7 +60,7 @@ export const LogoutBottomSheet = ({
         </Button>
         <div className="h-12px" />
         <div className="w-full text-center">
-          <Text typo="body" color="gray10" onClick={() => onClick()}>
+          <Text typo="body" color="gray10" onClick={() => onClose()}>
             취소
           </Text>
         </div>
