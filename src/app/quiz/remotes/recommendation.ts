1d9e6539336e5c35db3cfffa0210433c7aa86c02
dengoyoon <engus927@naver.com>
2023-09-13 02:15:40 +0900

    ♻️ refactor: 퀴즈 추천 데이터 캐싱 옵션을 no-cache 옵션으로 변경

--- src/app/quiz/remotes/recommendation.ts
+++ src/app/quiz/remotes/recommendation.ts
@@ -4,7 +4,7 @@ export const getRecommendationByQuizId = async (quizId: string) => {
   const result = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}api/v1/rec/quizzes?quizId=${quizId}`,
     {
-      cache: 'force-cache',
+      cache: 'no-cache',
     }
   );
   const quizRecommendInfo = await result.json();
