66397ba8e4f1d1e9911492d098480643fab83aa5
chaaerim <cofla411@ewhain.net>
2023-10-20 11:15:45 +0900

    :sparkles: feat: hotjar 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,21 @@ export default function RootLayout({
           `,
           }}
         />
+        {/* hotjar 설정 */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HJID},hjsv:${process.env.NEXT_PUBLIC_HJSV}};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+`,
+          }}
+        />
       </head>
       <body
         className={clsx(
