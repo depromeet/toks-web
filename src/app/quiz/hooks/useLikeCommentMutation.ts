88a4a1c78fcba4b42f72fa46a35ca19a82943ec0
chaaerim <cofla411@ewhain.net>
2023-11-12 22:02:44 +0900

    :sparkles: feat: useUnlikeCommentMutation에 optimistic update 적용

--- src/app/quiz/hooks/useLikeCommentMutation.ts
+++ src/app/quiz/hooks/useLikeCommentMutation.ts
@@ -25,18 +25,24 @@ export const useLikeCommentMutation = (commentId: string, quizId: string) => {
 
         const previousLiked: CommentListResponse | undefined =
           queryClient.getQueryData(QUERY_KEYS.GET_COMMENT_LIST(quizId));
-        console.log(previousLiked);
+
         queryClient.setQueryData(QUERY_KEYS.GET_COMMENT_LIST(quizId), () => {
           return {
             ...previousLiked,
             content: previousLiked?.content.map((el) => {
               if (el.id === Number(commentId)) {
-                el.isLiked = !el.isLiked;
+                return {
+                  ...el,
+                  isLiked: true,
+                  likeCount: el.likeCount + 1,
+                };
+              } else {
+                return el;
               }
-              return el;
             }),
           };
         });
+
         return { previousLiked };
       },
       onError: (err, previousLiked) => {
