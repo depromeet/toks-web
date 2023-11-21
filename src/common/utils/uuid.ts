7cae8c603dc3bc24dc602e4c44ea19ec610c24c9
chaaerim <cofla411@ewhain.net>
2023-11-21 23:17:01 +0900

    :sparkles: feat: add uuid function

---
+++ src/common/utils/uuid.ts
@@ -0,0 +1,8 @@
+export const uuidv4 = () => {
+  return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
+    (
+      Number(c) ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (Number(c) / 4)))
+    ).toString(16)
+  );
+};
