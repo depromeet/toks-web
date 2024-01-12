292fc867db5dcd02d6cf92c375a58ae77e130dc2
socar-riaco <riaco@socar.kr>
2024-01-12 23:10:18 +0900

    fix: ci error

--- src/__test__/quiz/page.test.tsx
+++ src/__test__/quiz/page.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { expect, test } from 'vitest';
 
-import QuizPage from '@/app/quiz/page';
+import QuizPage from '@/app/(BackHeader)/quiz/page';
 
 test('MyPage', () => {
   render(<QuizPage />);
