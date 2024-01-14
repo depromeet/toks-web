e4080c184665c2128112a182679f17548789f2ad
socar-riaco <riaco@socar.kr>
2024-01-14 15:40:24 +0900

    fix: ci error fix

--- src/common/constants/url.ts
+++ src/common/constants/url.ts
@@ -1,4 +1,5 @@
-export const API_URL = process.env.NEXT_PUBLIC_BASE_URL;
+export const API_URL =
+  process.env.NEXT_PUBLIC_BASE_URL ?? 'https://localhost:3000';
 export const LOGIN_URL = 'https://api.tokstudy.com/oauth2/authorize/kakao';
 export const GOOGLE_FORM_URL =
   'https://docs.google.com/forms/u/0/d/e/1FAIpQLSe_LaeFI1m8d96jhNcqmgFSogv_sazh0_lgpURXH0OXc326Ag/viewform?pli=1';
