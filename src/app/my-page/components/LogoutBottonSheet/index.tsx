718dad3ceb1f75322fce866a8ebe1d0b74c14fde
minsgy <alstjrchl97@likelion.org>
2024-02-14 22:12:58 +0900

    fix: order lint fix

--- src/app/my-page/components/LogoutBottonSheet/index.tsx
+++ src/app/my-page/components/LogoutBottonSheet/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 import { BottomSheet, Button, ICON_URL, Text } from '@/common';
 import { useLogoutQuery } from '@/queries';
-import { BottomSheetProps } from '@/store/types/bottomsheet';
+import { BottomSheetProps } from '@/types/bottomsheet';
 
 export const LogoutBottomSheet = ({ onClose, isShow }: BottomSheetProps) => {
   const { mutate: logoutMutation } = useLogoutQuery();
