109fd2fa095c6a1490fc620188dabfadfb75e2fa
chaaerim <cofla411@ewhain.net>
2024-02-16 22:57:34 +0900

    :sparkles: feat: notice slider component 구현

---
+++ src/common/components/NoticeSlider/index.tsx
@@ -0,0 +1,44 @@
+import { useRef } from 'react';
+import Slider from 'react-slick';
+
+import { Text } from '..';
+
+const setting = {
+  arrows: false,
+  dots: false,
+  autoplay: true,
+  infinite: true,
+  speed: 700,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
+
+type NoticeSliderProp = {
+  images: string[];
+  onClick: VoidFunction;
+};
+
+export const NoticeSlider = ({ images, onClick }: NoticeSliderProp) => {
+  const slider = useRef<Slider>(null);
+  return (
+    <Slider ref={slider} className="w-full" {...setting}>
+      {images?.map((url, idx) => (
+        <div
+          onClick={onClick}
+          className="relative h-auto rounded-12px bg-white"
+        >
+          <img
+            className="z-0 h-auto w-full rounded-12px"
+            alt="notice banner"
+            src={url}
+          />
+          <span className="absolute bottom-12px right-12px z-10 w-fit rounded-100px bg-gray-120 px-12px py-6px opacity-50">
+            <Text typo="caption">
+              {idx + 1}/{images.length}
+            </Text>
+          </span>
+        </div>
+      ))}
+    </Slider>
+  );
+};
