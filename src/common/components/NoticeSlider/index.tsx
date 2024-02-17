ddb5b33b037aae09bd0347cf255001f39b884a69
chaaerim <cofla411@ewhain.net>
2024-02-17 22:30:37 +0900

    :sparkles: feat: aspect ratio 속성 추가 및 배너 정보 위치 변경

--- src/common/components/NoticeSlider/index.tsx
+++ src/common/components/NoticeSlider/index.tsx
@@ -33,19 +33,25 @@ export const NoticeSlider = ({ images, url }: NoticeSliderProp) => {
       <Slider ref={slider} className="w-full" {...setting}>
         {image?.map((imageSrc, idx) => (
           <a
+            key={imageSrc}
             target="_blank"
             rel="noreferrer noopener"
             href={url}
             className="h-auto rounded-12px"
           >
             <img
-              className="z-0 h-auto w-full rounded-12px"
+              className="aspect-w-3 aspect-h-1 z-0 h-auto w-full rounded-12px"
               alt="notice banner"
               src={imageSrc}
             />
           </a>
         ))}
       </Slider>
+      {image.length > 1 && (
+        <span className="absolute bottom-12px right-12px z-10 w-fit rounded-100px bg-gray-120 px-12px py-8px opacity-50">
+          <Text typo="caption">1/{image.length}</Text>
+        </span>
+      )}
     </div>
   );
 };
