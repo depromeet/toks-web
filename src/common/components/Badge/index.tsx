ff2b3f6fdbcc7fba38158ced6a31e79bc3e12ad8
minsgy <alstjrchl97@gmail.com>
2024-02-22 22:06:04 +0900

    fix: badge 위치 수정

--- src/common/components/Badge/index.tsx
+++ src/common/components/Badge/index.tsx
@@ -4,7 +4,7 @@ import { Text } from '../Text';
 
 export const Badge = ({ label }: { label: string }) => {
   return (
-    <div className="inline-flex items-center justify-center rounded-[4px] bg-gray-110 px-[4px] py-[2px]">
+    <div className="inline-flex items-center justify-center rounded-[4px] bg-gray-110 px-[4px] pb-[2px] pt-[3px]">
       <Text typo="captionBold" color="white">
         {label}
       </Text>
