a5317aa0d4e467d6a51294a9f84a51024b8fb9ff
chaaerim <cofla411@ewhain.net>
2024-02-25 02:29:58 +0900

    :sparkles: feat: add event name

---
+++ src/common/constants/eventName.ts
@@ -0,0 +1,24 @@
+export const EVENT_NAME = {
+  // feat -> page -> at -> target -> action
+  toks: {
+    메인페이지: {
+      카테고리: {
+        카테고리: {
+          click: 'click_category',
+        },
+      },
+      플로팅버튼: {
+        플로팅버튼: {
+          click: 'click_floatingButton',
+        },
+      },
+    },
+    퀴즈페이지: {
+      더보기퀴즈: {
+        퀴즈카드: {
+          click: 'click_additionalQuizCard',
+        },
+      },
+    },
+  },
+};
