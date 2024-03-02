1512f514b1afde83fccf2174d10715869670d549
chaaerim <cofla411@ewhain.net>
2024-03-02 21:01:52 +0900

    :sparkles: feat: add getNotice api

--- src/app/toks-main/models/progress.ts
+++ src/app/toks-main/models/progress.ts
@@ -4,3 +4,16 @@ export interface ProgressResponse {
   description1: string;
   description2: string;
 }
+
+interface NoticeInfo {
+  id: number;
+  title: string;
+  seq: number;
+  imageUrl: string;
+  landingUrl: string;
+  isActive: boolean;
+}
+
+export interface NoticeResponse {
+  bottomBanners: NoticeInfo[];
+}
